68f55023f0f569aaaefff4c5e2ec58dbcfefcf71
yeddapenequito <2013056537@ust-ics.mygbiz.com>
2016-01-10 19:20:03 +0800

    Add about.php
    This is the php page for about us

---
+++ web/about.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<link rel='stylesheet' href='/bootstrap/css/bootstrap.min.css'/>
+<link rel="stylesheet" href='/bootstrap/css/bootstrap.css'/>
+<link rel="stylesheet" href='/bootstrap/css/bootstrap-responsive'/>
+<link rel="stylesheet" href='/bootstrap/css/bootstrap-responsive.min'/>
+<style>
+.text {
+    margin-left: auto;
+    margin-right: auto;
+	margin-top:-250px;
+    width:33em;
+	text-align: justify;
+}
+
+</style>
+</head>
+<body>
+<?php include 'header.php';?>
+<div id="container-fluid">
+	<div id="logo_1"><img src="/images/df970fe7-1b33-4ec9-afde-5e589b95db96.jpg" alt="Logo" style="width:250px;
+	height:80px; margin-top:70px; margin-left:50px;"></div>
+	<div id="logo_2"><img src="/images/07fa2be7-a87f-4d37-80a4-9f42330db1a6.png" alt="Logo" style="width:270px;
+	height:180px; margin-top:-10px; margin-left:40px;"></div>
+	<div class="text">
+	<p class="blocktext">
+	Lorem ipsum dolor sit amet, sapien etiam, nunc amet dolor ac odio mauris justo. Luctus arcu, urna praesent at id quisque ac. Arcu es massa vestibulum malesuada, integer vivamus elit eu mauris eus, cum eros quis aliquam wisi. Nulla wisi laoreet suspendisse integer vivamus elit eu mauris hendrerit facilisi, mi mattis pariatur aliquam pharetra eget.
+	</p>
+	<p class="blocktext">
+	Lorem ipsum dolor sit amet, sapien etiam, nunc amet dolor ac odio mauris justo. Luctus arcu, urna praesent at id quisque ac. Arcu es massa vestibulum malesuada, integer vivamus elit eu mauris eus, cum eros quis aliquam wisi. Nulla wisi laoreet suspendisse integer vivamus elit eu mauris hendrerit facilisi, mi mattis pariatur aliquam pharetra eget.
+	</p>
+	<p class="blocktext">
+	Lorem ipsum dolor sit amet, sapien etiam, nunc amet dolor ac odio mauris justo. Luctus arcu, urna praesent at id quisque ac. Arcu es massa vestibulum malesuada, integer vivamus elit eu mauris eus, cum eros quis aliquam wisi. Nulla wisi laoreet suspendisse integer vivamus elit eu mauris hendrerit facilisi, mi mattis pariatur aliquam pharetra eget.
+	</p>
+	</div>
+</div>	
+
+<?php include 'footer.php';?>
+<script type="text/javascript" src="/scripts/jquery-2.1.4.min.js"></script>
+<script type="text/javascript" src="/scripts/bootstrap.min.js"></script>
+<script type="text/javascript" src="/scripts/bootstrap.js"></script>
+</body>
+</html>
