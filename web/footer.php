4fece5fc282878092b13cee5f8dd3a59b18103a1
yeddapenequito <2013056537@ust-ics.mygbiz.com>
2016-01-10 19:16:04 +0800

    Add footer.php
    This is the footer that will be used in most of the sites

---
+++ web/footer.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<style>
+@font-face {
+        font-family: Roboto-Regular;
+        src: url(fonts/Roboto-Regular.ttf);
+}
+@font-face {
+         font-family:IOS8-Icons-Regular;
+        src: url(fonts/IOS8-Icons-Regular.tff);
+}
+header {
+    background-color:#153a7e;
+    color:white;
+    padding:5px; 
+}
+.contact{
+	text-align:left;
+	display:inline-block;
+}
+.employee{
+	text-align:right;
+	display:inline-block;
+}
+.site-footer{
+    background-color:#132c55;
+    color:white;
+    clear:both;
+    text-align:center;
+    padding:5px;
+	bottom:0;
+	position:absolute;
+	width:100%;
+	width:1350px;
+	margin-left:0px;
+}
+</style>
+</head>
+
+
+<body>
+<footer class="site-footer">
+(c) Machinovate International Incorporated 2016
+</footer>
+</body>
+</html>
