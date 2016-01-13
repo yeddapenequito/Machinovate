52324e6783da416494f7a87d73423678a72492f9
yeddapenequito <2013056537@ust-ics.mygbiz.com>
2016-01-13 15:55:12 +0800

    Add sitemap.php (without google maps yet)
    This is the initial page for sitemap.php

---
+++ web/sitemap.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<link rel='stylesheet' href='/bootstrap/css/bootstrap.min.css'/>
+<link rel="stylesheet" href='/bootstrap/css/bootstrap.css'/>
+<link rel="stylesheet" href='/bootstrap/css/bootstrap-responsive'/>
+<link rel="stylesheet" href='/bootstrap/css/bootstrap-responsive.min'/>
+<style>
+.text {
+    margin-left: auto;
+	margin-right: 30px;
+	margin-top:60px;
+    width:29em;
+	text-align: justify;
+}
+
+</style>
+</head>
+<body>
+<?php include 'header.php';?>
+<div id="container-fluid">
+	
+	<div class="text">
+	<p><b>MACHINOVATE INTERNATIONAL INC.</b></p>
+	<p><b>Address</b></p>
+    <p class="blocktext">Unit # 301-A King Center Bldg. Sgt Rivera St.Brgy. Manresa 
+	Quezon City Philippines 1115</p>
+	<p><b>Contact Details</b></p>
+	<p><b>Telephone No.</b></p>
+	<p>+632 404 6676</p>
+	<p>+632 623 2988</p>
+	<p>+632 423 2988</p>
+	<p><b>E-mail</b></p>
+	<p>machinovate.international@gmail.com</p>
+	</div>
+</div>	
+
+<!--<?php include 'footer.php';?>-->
+<script type="text/javascript" src="/scripts/jquery-2.1.4.min.js"></script>
+<script type="text/javascript" src="/scripts/bootstrap.min.js"></script>
+<script type="text/javascript" src="/scripts/bootstrap.js"></script>
+</body>
+</html>
