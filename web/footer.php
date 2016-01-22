6c2f8e2ab9f0e2a52229359e8b7c4c223ea0bcf7
Nyries <2013056563@ust-ics.mygbiz.com>
2016-01-22 09:48:28 +0800

    Revert "Delete link to footer.css"
    This reverts commit 1d2322d29f69839573906875f9d2cfef6e07020b.

--- web/footer.php
+++ web/footer.php
@@ -4,35 +4,10 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title></title>
-		<style>
-			html {
-			position: relative;
-			min-height: 100%;
-			}
-			body {
-			/* based from footer height */
-			margin-bottom: 35px;
-			}
-			footer {
-			position: absolute;
-			bottom: 0;
-			clear: both;
-			width: 100%;
-			height: 35px;
-			text-align: center;
-			color: white;
-			background-color: #132c55;
-			}
-			/*
-			Code above is modified and obtained from	https://getbootstrap.com/examples/sticky-footer/sticky-footer.css
-			*/
-		</style>
 	</head>
 	<body>
 		<footer>
-			<div class="container">
-				<p class="text-muted">&copy; Machinovate International Incorporated 2016<p>
-			<div>
+			&copy; Machinovate International Incorporated 2016
 		</footer>
 	</body>
-</html>				
+</html>
