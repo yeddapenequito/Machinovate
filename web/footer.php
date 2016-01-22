1a9e71bd765559ed47bf2a8f1bfa20679afea5fb
Nyries <2013056563@ust-ics.mygbiz.com>
2016-01-22 10:14:46 +0800

    Add main.css

--- web/footer.php
+++ web/footer.php
@@ -3,11 +3,13 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="./main.css">
 		<title></title>
 	</head>
 	<body>
 		<footer>
-			&copy; Machinovate International Incorporated 2016
+			<p class="text-center">&copy; Machinovate International Incorporated 2016</p>
 		</footer>
 	</body>
 </html>
