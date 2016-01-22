1d2322d29f69839573906875f9d2cfef6e07020b
Nyries <2013056563@ust-ics.mygbiz.com>
2016-01-22 09:47:28 +0800

    Delete link to footer.css
    Because footer.css was already deleted.

--- web/footer.php
+++ web/footer.php
@@ -5,10 +5,35 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="footer.css">
 		<title></title>
+		<style>
+			html {
+			position: relative;
+			min-height: 100%;
+			}
+			body {
+			/* based from footer height */
+			margin-bottom: 35px;
+			}
+			footer {
+			position: absolute;
+			bottom: 0;
+			clear: both;
+			width: 100%;
+			height: 35px;
+			text-align: center;
+			color: white;
+			background-color: #132c55;
+			}
+			/*
+			Code above is modified and obtained from	https://getbootstrap.com/examples/sticky-footer/sticky-footer.css
+			*/
+		</style>
 	</head>
 	<body>
 		<footer>
-			&copy; Machinovate International Incorporated 2016
+			<div class="container">
+				<p class="text-muted">&copy; Machinovate International Incorporated 2016<p>
+			<div>
 		</footer>
 	</body>
-</html>
+</html>				
