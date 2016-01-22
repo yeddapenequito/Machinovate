6772b716a1f94a6f77d7edec93d44269a550c2cf
Nyries <2013056563@ust-ics.mygbiz.com>
2016-01-22 08:51:14 +0800

    Delete style tag and its contents
    The same styles is found on footer.css.
    
    Note:
    The css file should be linked to footer.php.

--- web/footer.php
+++ web/footer.php
@@ -4,41 +4,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title></title>
-<style>
-@font-face {
-        font-family: Roboto-Regular;
-        src: url(fonts/Roboto-Regular.ttf);
-}
-@font-face {
-         font-family:IOS8-Icons-Regular;
-        src: url(fonts/IOS8-Icons-Regular.tff);
-}
-header {
-    background-color:#153a7e;
-    color:white;
-    padding:5px; 
-}
-.contact{
-	text-align:left;
-	display:inline-block;
-}
-.employee{
-	text-align:right;
-	display:inline-block;
-}
-.site-footer{
-    background-color:#132c55;
-    color:white;
-    clear:both;
-    text-align:center;
-    padding:5px;
-	bottom:0;
-	position:absolute;
-	width:100%;
-	width:1350px;
-	margin-left:0px;
-}
-</style>
 </head>
 
 
