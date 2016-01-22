5f4c01315ac5b2cea4833d264a55038356c285f3
Nyries <2013056563@ust-ics.mygbiz.com>
2016-01-22 09:08:37 +0800

    Reindent footer.php to improve readability

--- web/footer.php
+++ web/footer.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="footer.css">
-<title></title>
-</head>
-<body>
-<footer>
-	&copy; Machinovate International Incorporated 2016
-</footer>
-</body>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="footer.css">
+		<title></title>
+	</head>
+	<body>
+		<footer>
+			&copy; Machinovate International Incorporated 2016
+		</footer>
+	</body>
 </html>
