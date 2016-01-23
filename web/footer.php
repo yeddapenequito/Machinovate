5d1419ced09bad0d635749d3815322335d16fb4a
Nyries <2013056563@ust-ics.mygbiz.com>
2016-01-23 08:14:33 +0800

    Remove unnecessary tags(html, head, body) in footer.php

--- web/footer.php
+++ web/footer.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="./styles/bootstrap.min.css">
-		<link rel="stylesheet" href="./main.css">
-		<title></title>
-	</head>
-	<body>
-		<footer>
-			<p class="text-center">&copy; Machinovate International Incorporated 2016</p>
-		</footer>
-	</body>
-</html>
+<footer>
+	<p class="text-center">&copy; Machinovate International Incorporated 2016</p>
+</footer>
