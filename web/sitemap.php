1767ed9cf2b1051d3b004e2b6ba01f71b55261d2
Nyries <2013056563@ust-ics.mygbiz.com>
2016-01-31 17:50:41 +0800

    Modify sitemap

--- web/sitemap.php
+++ web/sitemap.php
@@ -3,52 +3,63 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="/web/styles/styles/bootstrap.min.css">
-		<link rel="stylesheet" href="/web/styles/main.css">
-		<title></title>
-		<style>
-			.text {
-			
-			margin-left: auto;
-			margin-right:50px;
-			margin-top:-400px;
-			width:29em;
-			text-align: justify;
+		<link rel="stylesheet" href="./styles/bootstrap.min.css">
+		<link rel="stylesheet" href="./styles/main.css">
+		<title>Machinovate | Sitemap</title>
+		<style type="text/css">
+			#content {
+				margin-top: 2em;
 			}
-			.sitemap{
-			
-			margin-left:80px;
-			margin-top: 30px;
+			#content address .page-header {
+				font-variant: small-caps;
+				font-weight: bold;
 			}
-			
+			#content address dl dt,
+			#content address dl dd {
+				padding: 1em;
+			}
+			#content address dl dt {
+				text-align: left;
+				color: #132c55;
+			}
+			#content address .glyphicon {
+				padding-right: 1em;
+			}
 		</style>
 	</head>
 	<body>
 		<?php include 'header.php';?>
-		<div id="container-fluid">
-			<div class="sitemap">
-				<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
-				width="700" height="440" src="https://maps.google.com/maps?hl=en&q= King Center Bldg, Sgt.
-				Rivera St, Quezon City, Metro Manila, Philippines&ie=UTF&&t=roadmap&z=19&iwloc=B&output=embed"></iframe>
-			</div>
-			<div class="text">
-				<p><b>MACHINOVATE INTERNATIONAL INC.</b></p>
-				<p><b>Address</b></p>
-				<p class="blocktext">Unit # 301-A King Center Bldg. Sgt Rivera St.Brgy. Manresa 
-				Quezon City Philippines 1115</p>
-				<p><b>Contact Details</b></p>
-				<p><b>Telephone No.</b></p>
-				<p>+632 404 6676</p>
-				<p>+632 623 2988</p>
-				<p>+632 423 2988</p>
-				<p><b>E-mail</b></p>
-				<p>machinovate.international@gmail.com</p>
+
+		<div id="content" class="container">
+			<div class="row">
+				<div class="col-sm-12 col-md-8 col-lg-7">
+					<iframe class="text-center" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.2098541594223!2d120.99441391418777!3d14.644025789774798!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b67b7a1ce8d1%3A0x5570c43b3bf78ccd!2s301%2C+King+Center+Bldg%2C+Sgt.+Rivera+St%2C+Quezon+City%2C+Metro+Manila!5e0!3m2!1sen!2sph!4v1454231280176" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</div>
+				<address class="col-sm-12 col-md-4 col-lg-5">
+					<h1 class="page-header">Contact Information</h1>
+					<dl class="dl-horizontal">
+						<dt><span class="glyphicon glyphicon-road"></span>Address</dt>
+						<dd>Unit # 301-A King Center Bldg. Sgt Rivera St.Brgy. Manresa Quezon City Philippines 1115</dd>
+						<dt><span class="glyphicon glyphicon-phone-alt"></span>Telephone</dt>
+						<dd>
+							<ul class="list-unstyled">
+								<li>+632 404 6676</li>
+								<li>+632 623 2988</li>
+								<li>+632 423 2988</li>
+							</ul>
+						</dd>
+						<dt><span class="glyphicon glyphicon-envelope"></span>E-mail</dt>
+						<dd><a href="mailto:machinovate.international@gmail.com">machinovate.international@gmail.com</a></dd>
+					</dl>
+				</address>
 			</div>
 		</div>	
 		
 		<!--<?php include 'footer.php';?>-->
 		<script type="text/javascript" src="/scripts/jquery-2.1.4.min.js"></script>
 		<script type="text/javascript" src="/scripts/bootstrap.min.js"></script>
-		<script type="text/javascript" src="/scripts/bootstrap.js"></script>
+		<script type="text/javascript">
+			document.getElementById("sitemap").className = "active";
+		</script>
 	</body>
 </html>
