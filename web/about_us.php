6e7b33f9c82e873876dfd7787d92941f155bc65f
Nyries <2013056563@ust-ics.mygbiz.com>
2016-01-31 21:48:29 +0800

    Modify about_us.php

--- web/about_us.php
+++ web/about_us.php
@@ -9,30 +9,33 @@
 	</head>
 	<body>
 		<?php include 'header.php';?>
-		<div id="container-fluid">
-			<div id="logo_1"><img src="/images/df970fe7-1b33-4ec9-afde-5e589b95db96.jpg" alt="Logo" style="width:250px;
-			height:80px; margin-top:70px; margin-left:50px;"></div>
-			<div id="logo_2"><img src="/images/07fa2be7-a87f-4d37-80a4-9f42330db1a6.png" alt="Logo" style="width:270px;
-			height:180px; margin-top:-10px; margin-left:40px;"></div>
-			<div class="text">
-				<p class="blocktext">
-					Lorem ipsum dolor sit amet, sapien etiam, nunc amet dolor ac odio mauris justo. Luctus arcu, urna praesent at id quisque ac. Arcu es massa vestibulum malesuada, integer vivamus elit eu mauris eus, cum eros quis aliquam wisi. Nulla wisi laoreet suspendisse integer vivamus elit eu mauris hendrerit facilisi, mi mattis pariatur aliquam pharetra eget.
-				</p>
-				<p class="blocktext">
-					Lorem ipsum dolor sit amet, sapien etiam, nunc amet dolor ac odio mauris justo. Luctus arcu, urna praesent at id quisque ac. Arcu es massa vestibulum malesuada, integer vivamus elit eu mauris eus, cum eros quis aliquam wisi. Nulla wisi laoreet suspendisse integer vivamus elit eu mauris hendrerit facilisi, mi mattis pariatur aliquam pharetra eget.
-				</p>
-				<p class="blocktext">
-					Lorem ipsum dolor sit amet, sapien etiam, nunc amet dolor ac odio mauris justo. Luctus arcu, urna praesent at id quisque ac. Arcu es massa vestibulum malesuada, integer vivamus elit eu mauris eus, cum eros quis aliquam wisi. Nulla wisi laoreet suspendisse integer vivamus elit eu mauris hendrerit facilisi, mi mattis pariatur aliquam pharetra eget.
-				</p>
+
+		<div class="content" class="container">
+			<div class="row">
+				<div class="col-xs-4 col-sm-3">
+					<img class="logo" src="./images/machinovate-logo-2.png">
+					<img class="logo" src="./images/machinovate-logo-3.png">
+				</div>
+				<div class="col-xs-8 col-sm-9">
+					<p class="text-justify lead">
+						Lorem ipsum dolor sit amet, sapien etiam, nunc amet dolor ac odio mauris justo. Luctus arcu, urna praesent at id quisque ac. Arcu es massa vestibulum malesuada, integer vivamus elit eu mauris eus, cum eros quis aliquam wisi. Nulla wisi laoreet suspendisse integer vivamus elit eu mauris hendrerit facilisi, mi mattis pariatur aliquam pharetra eget.
+					</p>
+					<p class="text-justify">
+						Lorem ipsum dolor sit amet, sapien etiam, nunc amet dolor ac odio mauris justo. Luctus arcu, urna praesent at id quisque ac. Arcu es massa vestibulum malesuada, integer vivamus elit eu mauris eus, cum eros quis aliquam wisi. Nulla wisi laoreet suspendisse integer vivamus elit eu mauris hendrerit facilisi, mi mattis pariatur aliquam pharetra eget.
+					</p>
+					<p class="text-justify">
+						Lorem ipsum dolor sit amet, sapien etiam, nunc amet dolor ac odio mauris justo. Luctus arcu, urna praesent at id quisque ac. Arcu es massa vestibulum malesuada, integer vivamus elit eu mauris eus, cum eros quis aliquam wisi. Nulla wisi laoreet suspendisse integer vivamus elit eu mauris hendrerit facilisi, mi mattis pariatur aliquam pharetra eget.
+					</p>
+				</div>
 			</div>
 		</div>	
 		
 		<?php include 'footer.php';?>
 
-		<script type="text/javascript" src="/web/scripts/jquery-2.2.0.min.js"></script>
-		<script type="text/javascript" src="/web/scripts/bootstrap.min.js"></script>
+		<script type="text/javascript" src="./scripts/jquery-2.2.0.min.js"></script>
+		<script type="text/javascript" src="./scripts/bootstrap.min.js"></script>
 		<script type="text/javascript">
-			document.getElementById("about-us").className = "active";
+			document.getElementById("about_us").className = "active";
 		</script>
 	</body>
 </html>
--- web/sitemap.php
+++ web/sitemap.php
@@ -6,31 +6,11 @@
 		<link rel="stylesheet" href="./styles/bootstrap.min.css">
 		<link rel="stylesheet" href="./styles/main.css">
 		<title>Machinovate | Sitemap</title>
-		<style type="text/css">
-			#content {
-				margin-top: 2em;
-			}
-			#content address .page-header {
-				font-variant: small-caps;
-				font-weight: bold;
-			}
-			#content address dl dt,
-			#content address dl dd {
-				padding: 1em;
-			}
-			#content address dl dt {
-				text-align: left;
-				color: #153a7e;
-			}
-			#content address .glyphicon {
-				padding-right: 1em;
-			}
-		</style>
 	</head>
 	<body>
 		<?php include 'header.php';?>
 
-		<div id="content" class="container">
+		<div id="map" class="content" class="container">
 			<div class="row">
 				<div class="col-sm-12 col-md-8 col-lg-7">
 					<iframe class="text-center" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.2098541594223!2d120.99441391418777!3d14.644025789774798!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b67b7a1ce8d1%3A0x5570c43b3bf78ccd!2s301%2C+King+Center+Bldg%2C+Sgt.+Rivera+St%2C+Quezon+City%2C+Metro+Manila!5e0!3m2!1sen!2sph!4v1454231280176" width="575" height="425" frameborder="0" style="border:0" allowfullscreen></iframe>
@@ -57,8 +37,8 @@
 		
 		<?php include 'footer.php';?>
 
-		<script type="text/javascript" src="/scripts/jquery-2.2.0.min.js"></script>
-		<script type="text/javascript" src="/scripts/bootstrap.min.js"></script>
+		<script type="text/javascript" src="./scripts/jquery-2.2.0.min.js"></script>
+		<script type="text/javascript" src="./scripts/bootstrap.min.js"></script>
 		<script type="text/javascript">
 			document.getElementById("sitemap").className = "active";
 		</script>
