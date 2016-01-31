ea8ad31d651533b9714ec9b04f2526856cfc0b6e
yeddapenequito <2013056537@ust-ics.mygbiz.com>
2016-01-31 18:26:02 +0800

    Organized table for slitter specs

--- web/slitter.php
+++ web/slitter.php
@@ -3,114 +3,136 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="/web/styles/bootstrap.min.css">
-		<link rel="stylesheet" href="/web/styles/main.css">
+		<link rel="stylesheet" href="./styles/bootstrap.min.css">
+		<link rel="stylesheet" href="./styles/main.css">
 		<title></title>
 		<style>
 			
 			.table-hover{
 				position:absolute;
-				margin-top:30px;
+				margin-top:40px;
 				margin-right:auto;
 				margin-left:30px;
 			}
 			th{
 				color:blue;
-				text-align:center;
+				text-align:left;
 			}
 			.row h4{
-				
+				text-align: justify;
 				margin-left:500px;
 			}
 
+			.button{
+				margin-top: 10px;
+				margin-bottom: 10px;
+			}
 		</style>
 	</head>
 	<body>
 		<?php include 'header.php';?>
 		<div id="container-fluid">
-			<div class="row">
-			<h4>SINGLE-FLY Sheeting Machine</h4>
-			</div>
+			
 		
-	      <table class="table table-hover" style="width:90%">
+	      <table class="table table-hover" style="width:90%" border="1">
 			    <thead>
 			      <tr>
-			        <th style="text-align:center;">MODEL <p style="text-align:center;">(Bigger Sizes Available)</p></th>
-			        <th>MACH-SRV 56</th>
-			        <th>MACH-SRV 66</th>
-			        <th>MACH-SRV 80</th>
+			        <th>MODEL</th>
+			        <th>MSR 70</th>
+			        <th>MSR 90</th>
+			        <th>MSR 110</th>
+			        <th>MSR 130</th>
 			        
 			      </tr>
 			    </thead>
 			<tbody>
 		      <tr><!--limitation: auto create tables?-->
-		     		<td>Max. Web Width</td>
-		     		<td>56" (1420mm) </td>
-					<td>66" (1680mm) </td>
-					<td>80" (2000mm) </td>
+		     		<td>Max. Unwind  Reel Width </td>
+		     		<td>1780 mm </td>
+					<td>2280 mm </td>
+					<td>2800 mm </td>
+					<td>3300 mm</td>
 		     	</tr>
-	         <tr>   <td>Max. Unwinding Diameter</td>
-	         		<td></td>
-	         		<td></td>
-	         		<td></td>
+	         <tr>   <td>Designed Speed</td>
+	         		<td>600 M/Min</td>
+	         		<td>600 M/Min</td>
+	         		<td>800 M/Min</td>
+	         		<td>800 M/Min</td>
 	         		 
 	         </tr>
-			 <tr>	<td>H-Type (Min.4 reels) </td>
-			 		<td> 1300 mm x 4-20reels</td>
-			 		<td> 1300 mm x 4-20reels</td>
-			 		<td> 1500 mm x 4-20reels</td>
+			 <tr>	<td>Maximum Unwinding Diamter</td>
+			 		<td>1800 mm</td>
+			 		<td>1800 mm</td>
+			 		<td>1800 mm</td>
+			 		<td>1800 mm</td>
 
 
 			 </tr>
-			 <tr>	<td>Hydraulic Shaftless(Single &Double) </td>
-			 		<td>1500 mm x 2-6 reels</td>
-			 		<td>1500 mm x 2-6 reels</td>
-			 		<td>1800 mm x 2-6 reels</td>
+			 <tr>	<td>Maximum Rewinding Diameter</td>
+			 		<td>1500 mm</td>
+			 		<td>1500 mm</td>
+			 		<td>1500 mm</td>
+			 		<td>1500 mm</td>
 
 
 			 </tr>
-			 <tr>	<td>Designed Speed(varies on Cut length and Materials) </td>
-			 		<td>300M/Min.</td>
-			 		<td>300M/Min.</td>
-			 		<td>300M/Min.</td>
+			 <tr>	<td>Core Diameter of Rewinding Reel</td>
+			 		<td>76.20-305 mm</td>
+			 		<td>76.20-305 mm</td>
+			 		<td>76.20-305 mm</td>
+			 		<td>76.20-305 mm</td>
 
 			 </tr>
-			 <tr>	<td>Min.- Max. knife load </td>
-			 		<td>40-500gsm </td>
-			 		<td>40-500gsm </td>
-			 		<td>40-500gsm </td>
+			 <tr>	<td></td>
+			 		<td>( 3" - 12" )</td>
+			 		<td>( 3" - 12" )</td>
+			 		<td>( 3" - 12" )</td>
+			 		<td>( 3" - 12" )</td>
 			 </tr>
-			 <tr>	<td>Sheet length range(Longer length upon request)</td>
-			 		<td>( 380-1700mm) </td>
-			 		<td>( 380-1700mm) </td>
-			 		<td>( 380-1700mm) </td>
+			 <tr>	<td>Core Diameter of Unwinding Reel</td>
+			 		<td>76.20-305 mm</td>
+			 		<td>76.20-305 mm</td>
+			 		<td>76.20-305 mm</td>
+			 		<td>76.20-305 mm</td>
 			 </tr>
-			 <tr>	<td>Cutting Tolerance</td>
-			 		<td>±0.5mm</td>
-			 		<td>±0.5mm</td>
-			 		<td>±0.5mm</td>
+			 <tr>	<td></td>
+			 		<td>( 3" - 12" )</td>
+			 		<td>( 3" - 12" )</td>
+			 		<td>( 3" - 12" )</td>
+			 		<td>( 3" - 12" )</td>
 			 </tr>
-			 <tr>	<td>Max. Pile Height (incl. Pallet)(can request Hi-Pile up to 1700mm) </td>
-			 		<td>51"(1300mm) </td>
-			 		<td>51"(1300mm) </td>
-			 		<td>51"(1300mm) </td>
+			 <tr>	<td>Total Power Requirement</td>
+			 		<td>220/380/440 Volts</td>
+			 		<td>220/380/440 Volts</td>
+			 		<td>220/380/440 Volts</td>
+			 		<td>220/380/440 Volts</td>
 			 </tr>
-			 <tr>	<td>Max. Pile Weight </td> 
-			 		<td>1500kgs. </td>
-			 		<td>1500kgs. </td>
-			 		<td>2200kgs. </td>
+			 <tr>	<td></td>
+			 		<td>( 25 kW)</td>
+			 		<td>( 29 kW)</td>
+			 		<td>( 33 kW)</td>
+			 		<td>( 33 kW)</td>
+			 </tr>
+			 <tr>	<td>Machine Dimension</td> 
+			 		<td>7.7 MX  4M  X 3.25M</td>
+			 		<td>7.7 MX  4M  X 3.25M</td>
+			 		<td>7.7 MX  5.5 X 3.25M</td>
+			 		<td>7.7 MX  6M  X 3.25M</td>
 			 </tr>
 		    </tbody>
 		  </table>
                             <!--df-->
- 				
+ 				<div class="button">
+					
+					<a href="/web/order_slitter.php" class="btn btn-md btn-info btn-block" role="button">Order Slitter</a>
+				</div>
 
  				
 		</div>	
 		
 		<?php include 'footer.php';?>
-		<script type="text/javascript" src="/web/scripts/jquery-2.1.4.min.js"></script>
-		<script type="text/javascript" src="/web/scripts/bootstrap.min.js"></script>
-		<script type="text/javascript" src="/web/scripts/bootstrap.js"></script>
+		<script type="text/javascript" src="./scripts/jquery-2.1.4.min.js"></script>
+		<script type="text/javascript" src="./scripts/bootstrap.min.js"></script>
+		<script type="text/javascript" src="./scripts/bootstrap.js"></script>
 	</body>
 </html>
