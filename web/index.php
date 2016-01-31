ea762e347a2b2f0f35e7286fc4c04019d12cb158
Nyries <2013056563@ust-ics.mygbiz.com>
2016-01-31 14:56:30 +0800

    Change default background-color of navbar menu items upon mouse hover

--- web/index.php
+++ web/index.php
@@ -10,45 +10,61 @@
 	<body>
 		<?php include 'header.php';?>
 		
-		<!-- carousel -->
-		<!--<div id="my-carousel" class="carousel slide" data-ride="carousel">
-			<ol class="carousel-indicators">
-				<li data-target="#my-carousel" data-slide-to="0" class="active"></li>
-				<li data-target="#my-carousel" data-slide-to="1"></li>
-				<li data-target="#my-carousel" data-slide-to="2"></li>
-			</ol>
-			<div class="carousel-inner" role="listbox">
-				<div class="item active">
-					<img alt="First slide" src="./images/cover-picture-1.jpg">
-					<div class="carousel-caption">
-						<p>Our innovation + The latest technology</p>
-						<h3>Providing you the best</h3>
-						<h2>Paper Converting Machines</h2>
-						<a href="#" class="btn btn-large btn-primary">View our Products</a>
-					</div>
-				</div>
-				<div class="item">
-					<img alt="Second slide" src="./images/cover-picture-2.jpg">
-					<div class="carousel-caption">
-						<p>Our innovation + The latest technology</p>
-						<h3>Providing you the best</h3>
-						<h2>Paper Converting Machines</h2>
-						<a href="#" class="btn btn-large btn-primary">View our Products</a>
-					</div>
-				</div>
-			    <div class="item">
-					<img alt="Third slide" src="./images/cover-picture-3.jpg">
-					<div class="carousel-caption">
-						<p>Our innovation + The latest technology</p>
-						<h3>Providing you the best</h3>
-						<h2>Paper Converting Machines</h2>
-						<a href="#" class="btn btn-large btn-primary">View our Products</a>
-					</div>
-				</div>
-			</div>
-		</div> -->
+		 <!-- Carousel
+	    ================================================== 
+		This code is obtained and modified from https://getbootstrap.com/examples/carousel/-->
+
+	    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+	      <!-- Indicators -->
+	      <ol class="carousel-indicators">
+	        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+	        <li data-target="#myCarousel" data-slide-to="1"></li>
+	        <li data-target="#myCarousel" data-slide-to="2"></li>
+	      </ol>
+	      <div class="carousel-inner" role="listbox">
+	        <div class="item active">
+	          <img class="first-slide" src="./images/cover-picture-1.jpg" alt="First slide">
+	          <div class="container">
+	            <div class="carousel-caption">
+	              <h1>Example headline.</h1>
+	              <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+	              <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+	            </div>
+	          </div>
+	        </div>
+	        <div class="item">
+	          <img class="second-slide" src="./images/cover-picture-2.jpg" alt="Second slide">
+	          <div class="container">
+	            <div class="carousel-caption">
+	              <h1>Another example headline.</h1>
+	              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+	              <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+	            </div>
+	          </div>
+	        </div>
+	        <div class="item">
+	          <img class="third-slide" src="./images/cover-picture-3.jpg" alt="Third slide">
+	          <div class="container">
+	            <div class="carousel-caption">
+	              <h1>One more for good measure.</h1>
+	              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+	              <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+	            </div>
+	          </div>
+	        </div>
+	      </div>
+	      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+	        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+	        <span class="sr-only">Previous</span>
+	      </a>
+	      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+	        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+	        <span class="sr-only">Next</span>
+	      </a>
+	    </div><!-- /.carousel -->
 		
 		<?php include 'footer.php'; ?>
+
 		<script type="text/javascript" src="./scripts/jquery-2.2.0.min.js"></script>
 		<script type="text/javascript" src="./scripts/bootstrap.min.js"></script>
 		<script type="text/javascript">
