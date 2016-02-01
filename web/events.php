0b393a9260b3caeadfa2b576fc19bea4bd214317
Nyries <2013056563@ust-ics.mygbiz.com>
2016-02-01 08:17:46 +0800

    Modify events.php

--- web/events.php
+++ web/events.php
@@ -3,95 +3,66 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="/web/styles/bootstrap.min.css">
-		<link rel="stylesheet" href="/web/styles/main.css">
-		<title></title>
-		<style>
-			.text {
-			
-			margin-left: auto;
-			margin-right:auto;
-			margin-top:40px;
-			width:40em;
-			text-align: justify;
-			}
-			.image1{
-			position:absolute;
-			}
-			.image2{
-			position:absolute;
-			}
-			.image3{
-			position:absolute;
-			}
-			.three_w{
-			position:absolute;
-			margin-right:50px;
-			}
-			.sidetext{
-			position:absolute;
-			margin-top:-480px;
-			margin-left:50px;
-			}
-			.three_w{
-			position:absolute;
-			margin-top:-440px;
-			margin-left:110px;
-			}
-			.events{
-			position:absolute;
-			margin-top:-300px;
-			margin-left:50px;
-			}
-		</style>
+		<link rel="stylesheet" href="./styles/bootstrap.min.css">
+		<link rel="stylesheet" href="./styles/main.css">
+		<title>Machinovate | Events</title>
 	</head>
 	<body>
 		<?php include 'header.php';?>
-		<div id="container-fluid">
-			
-			<div class="text">
-				<h1><b>Event Name</b></h1>
-				<p><b>Event Description</b></p>
-				<div id="image1"><img src="/images/GeneratedCross-0.png" alt="Logo" style="width:180px;
-				height:150px; margin-top:10px; margin-left:0px;"></div>
-				<div id="image2"><img src="/images/GeneratedCross-0.png" alt="Logo" style="width:180px;
-				height:150px; margin-top:-170px; margin-left:210px;"></div>
-				<div id="image3"><img src="/images/GeneratedCross-0.png" alt="Logo" style="width:180px;
-				height:150px; margin-top:-210px; margin-left:420px;"></div>
-				<p class="blocktext" style="margin-left:20px;">
-					Lorem ipsum dolor sit amet, sapien etiam, nunc amet dolor ac odio mauris justo. Luctus arcu, urna praesent at id quisque ac. Arcu es massa vestibulum malesuada, integer vivamus elit eu mauris eus, cum eros quis aliquam wisi. Nulla wisi laoreet suspendisse integer vivamus elit eu mauris hendrerit facilisi, mi mattis pariatur aliquam pharetra eget.
-				</p>
-				<p class="blocktext" style="margin-left:20px;">
-					Lorem ipsum dolor sit amet, sapien etiam, nunc amet dolor ac odio mauris justo. Luctus arcu, urna praesent at id quisque ac. Arcu es massa vestibulum malesuada, integer vivamus elit eu mauris eus, cum eros quis aliquam wisi. Nulla wisi laoreet suspendisse integer vivamus elit eu mauris hendrerit facilisi, mi mattis pariatur aliquam pharetra eget.
-				</p>
-			</div>
-			<div class="sidetext">
-				<p>Upcoming event this 2015..</p>
-				<div id="image1"><img src="/images/GeneratedCross-4.png" alt="Logo" style="width:50px;
-				height:50px; margin-top:10px; margin-left:auto;"></div>
-				
-			</div>
-			<div class="three_w">
-				<p><b>What</b>: Paperex India 2015</p>
-				<p><b>When</b>: Day 00 Month</p>
-				<p><b>Where</b>: Pragati, Maidan, New Delhi India</p>
-				<b style="margin-left:-70px;">_________________________________________</b>
-			</div>
-			<div class="events">
-				<p>Look at the past events here..</p>
-				<p>2014-Event Name</p>
-				<p>2013-Event Name</p>
-				<p>2012-Event Name</p>
-				<p>2011-Event Name</p>
-				<p>2010-Event Name</p>
-				<p>2009-Event Name</p>
-				
+
+		<div class="content" class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-3">
+					<div>
+						<p>Upcoming event this 2015..</p>
+						<div id="image1"><img src="/images/GeneratedCross-4.png" alt="Logo" style="width:50px;
+						height:50px; margin-top:10px; margin-left:auto;"></div>
+						
+					</div>
+					<div>
+						<p><b>What</b>: Paperex India 2015</p>
+						<p><b>When</b>: Day 00 Month</p>
+						<p><b>Where</b>: Pragati, Maidan, New Delhi India</p>
+						<b>________________________________</b>
+					</div>
+					<div>
+						<p>Look at the past events here..</p>
+						<p>2014-Event Name</p>
+						<p>2013-Event Name</p>
+						<p>2012-Event Name</p>
+						<p>2011-Event Name</p>
+						<p>2010-Event Name</p>
+						<p>2009-Event Name</p>
+						
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-9">
+					<header>
+						<h1 class="page-header">Event Name</h1>
+						<small>Event Description</small>
+					</header>
+					<img src="./images/GeneratedCross-0.png" alt="Logo">
+					<img src="./images/GeneratedCross-0.png" alt="Logo">
+					<img src="./images/GeneratedCross-0.png" alt="Logo">
+					<p>
+						Lorem ipsum dolor sit amet, sapien etiam, nunc amet dolor ac odio mauris justo. Luctus arcu, urna praesent at id quisque ac. Arcu es massa vestibulum malesuada, integer vivamus elit eu mauris eus, cum eros quis aliquam wisi. Nulla wisi laoreet suspendisse integer vivamus elit eu mauris hendrerit facilisi, mi mattis pariatur aliquam pharetra eget.
+					</p>
+					<p>
+						Lorem ipsum dolor sit amet, sapien etiam, nunc amet dolor ac odio mauris justo. Luctus arcu, urna praesent at id quisque ac. Arcu es massa vestibulum malesuada, integer vivamus elit eu mauris eus, cum eros quis aliquam wisi. Nulla wisi laoreet suspendisse integer vivamus elit eu mauris hendrerit facilisi, mi mattis pariatur aliquam pharetra eget.
+					</p>
+				</div>
 			</div>
 		</div>	
 		
 		<!--<?php include 'footer.php';?>-->
-		<script type="text/javascript" src="/scripts/jquery-2.1.4.min.js"></script>
-		<script type="text/javascript" src="/scripts/bootstrap.min.js"></script>
-		<script type="text/javascript" src="/scripts/bootstrap.js"></script>
+
+		<?php include 'footer.php'; ?>
+		<script type="text/javascript" src="./scripts/jquery-2.2.0.min.js"></script>
+		<script type="text/javascript" src="./scripts/bootstrap.min.js"></script>
+		<script type="text/javascript">
+			document.getElementById("events").className = "active";
+			  interval: 10000
+			});
+		</script>
 	</body>
 </html>
