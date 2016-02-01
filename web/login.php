2ca3ec3ec36c655d0df25251a7a71f8497dbbc6d
Nyries <2013056563@ust-ics.mygbiz.com>
2016-02-01 09:36:45 +0800

    Modify login.php

--- web/login.php
+++ web/login.php
@@ -5,48 +5,64 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="./styles/bootstrap.min.css">
 		<link rel="stylesheet" href="./styles/main.css">
-		<title></title>
-		<style>
-			.row h3{
-			margin-left:350px;
-			margin-top:60px;
-			position:absolute;
-			}
-			.row1{
-			margin-left:30px;
-			margin-top:100px;
-			
-			}
-		</style>
+		<title>Machinovate | Login</title>
 	</head>
 	<body>
-		<?php include 'header_2.php';?>
+		<?php include 'header.php';?>
+
 		<div class="container">
-			
 			<div class="row">
-				<h3>Login</h3>
-				<div class="row1">
-					<form role="form" action="processlogin.html" method="post" 
-					class="col-xs-offset-1 col-xs-10 col-sm-offset-3 col-sm-6 col-md-offset-4 col-md-4">
-						<p>Username</p>
+				<div class="row">
+					<form role="form" action="processlogin.html" method="post" class="col-xs-offset-1 col-xs-10 col-sm-offset-3 col-sm-6 col-md-offset-4 col-md-4">
+						
+						<h3 class="page-header">Login</h3>
+
 						<div class="form-group">
-							<input type="text" name="username" placeholder="Username" class="form-control" required autofocus> 
+							<label for="username">Username</label>
+							<input id="username" type="text" placeholder="Enter username" class="form-control" required autofocus> 
 						</div>
-						<p>Password</p>
+
 						<div class="form-group">
-							<input type="password" name="password" placeholder="Password" class="form-control" required> 
+							<label for="password">Password</label>
+							<input id="password" type="password" placeholder="Enter password" class="form-control" required> 
 						</div>
-						<input type="checkbox" name="remember" value="remember"> Remember Me<br>
-						<br>
-						<input type="submit" value="Submit" class="btn btn-primary"
-						id="form-button">
+
+						<div class="form-group">
+							<div class="checkbox">
+							  <label for="remember"><input id="remember" type="checkbox" value="remember">Remember Me</label>
+							</div>
+						</div>
+						
+						<!-- The following code for implementing captcha is obtained and modified from http://www.the-art-of-web.com/php/captcha/-->
+						
+						<div class="form-horizontal">
+							<div class="form-group">
+								<label for="captcha" class="control-label col-sm-5">
+									<img src="./captcha.php" alt="CAPTCHA">
+								</label>
+								<div class="col-sm-7">
+									<input id="captcha" type="text" name="captcha" class="form-control">
+								</div>
+								<small>Copy the digits from the image into this box</small>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<input id="login-button" type="submit" value="Login" class="btn btn-primary btn-block">
+						</div>
+
+						
 					</form>
 				</div>
 			</div>
 		</div>
+
 		<?php include 'footer.php';?>
-		<script type="text/javascript" src="/scripts/jquery-2.2.0.min.js"></script>
-		<script type="text/javascript" src="/scripts/bootstrap.min.js"></script>
-		<script type="text/javascript">
+
+		<script type="text/javascript" src="./scripts/jquery-2.2.0.min.js"></script>
+		<script type="text/javascript" src="./scripts/bootstrap.min.js"></script>
+		<script>
+			$("#employee-login").remove();
+		</script>
 	</body>
 </html>
