efb00e3856d18b12cf99344bfcefdc29b68012c9
Nyries <2013056563@ust-ics.mygbiz.com>
2016-02-10 19:10:42 +0800

    Add header_before_login.php and header_after_login.php

---
+++ web/header_after_login.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<div class="navbar-brand">
+				<a href="index.php"><img src="./images/machinovate-logo-white-no-border.png"></a>
+			</div>
+		</div>
+		<div id="navbar" class="navbar-collapse collapse">
+			<ul class="nav navbar-nav">
+				<li><a id="dashboard" href="dashboard.php">Dashboard</a></li>
+				<li><a id="events" href="events.php">Events</a></li>
+				<li><a id="reports" href="reports.php">Reports</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li>
+					<a id="employee-logout">
+						<span id="hello-user">Hello, admin</span>
+						<button id="logout-button" href="login.php" class="btn btn-primary btn-sm" role="button">Logout</button> 
+					</a>
+				</li>
+			</ul>
+		</div><!--/.nav-collapse -->
+	</div><!--/.container-fluid -->
+</nav>
---
+++ web/header_before_login.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<div class="navbar-brand">
+				<a href="index.php"><img src="./images/machinovate-logo-white-no-border.png"></a>
+			</div>
+		</div>
+		<div id="navbar" class="navbar-collapse collapse">
+			<ul class="nav navbar-nav">
+				<li><a id="home" href="index.php">Home</a></li>
+				<li><a id="machines" href="machines.php">Machines</a></li>
+				<li><a id="order-now" href="order_now.php">Order Now</a></li>
+				<li><a id="events" href="events.php">Events</a></li>
+				<li><a id="contact-us" href="contact_us.php">Contact Us</a></li>
+				<li><a id="agents" href="agents.php">Agents</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li>
+					<a id="employee-login">
+						<span id="are-you-an-employee">Are you an employee?</span>
+						<button id="login-button" href="login.php" class="btn btn-primary btn-sm" role="button">Login</button> 
+					</a>
+				</li>
+			</ul>
+		</div><!--/.nav-collapse -->
+	</div><!--/.container-fluid -->
+</nav>
