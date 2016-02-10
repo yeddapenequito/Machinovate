ecc67115f4702caed45d231863d2100836f84a2f
Nyries <2013056563@ust-ics.mygbiz.com>
2016-02-10 22:16:26 +0800

    Add comment to insert slideshow or carousel code on index.php

--- web/index.php
+++ web/index.php
@@ -10,6 +10,8 @@
 	<body>
 		<?php include 'header_before_login_home_only.php';?>
 		
+		<!--insert slideshow or carousel code here -->
+		
 		<script type="text/javascript" src="scripts/jquery-2.2.0.min.js"></script>
 		<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
 		<script>
