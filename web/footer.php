bc1b6bffcf9534c63eb85f02272d7584e2eb505f
Nyries <2013056563@ust-ics.mygbiz.com>
2016-02-10 22:14:08 +0800

    Delete unnecessary files

--- web/footer.php
+++
@@ -1,3 +0,0 @@
-<footer class="navbar-fixed-bottom">
-	<p class="text-center"><small>&copy; Machinovate International Inc. 2016<small></p>
-</footer>
--- web/header.php
+++
@@ -1,2 +0,0 @@
-<?php include 'header_top.php';?>
-<?php include 'header_bottom.php';?>										
