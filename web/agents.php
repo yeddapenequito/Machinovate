8fb9bec2d201325b99a154f35918c94b868b12be
Nyries <2013056563@ust-ics.mygbiz.com>
2016-02-12 14:47:03 +0800

    Add agents.php

---
+++ web/agents.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="styles/bootstrap.min.css">
+		<link rel="stylesheet" href="styles/main.css">
+		<title>Machinovate | Order Now</title>
+	</head>
+	<body>
+		<?php include 'header_before_login.php';?>
+
+		<script type="text/javascript" src="scripts/jquery-2.2.0.min.js"></script>
+		<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
+		<script type="text/javascript">
+			document.getElementById("agents").className = "active";
+		</script>
+	</body>
+</html>
