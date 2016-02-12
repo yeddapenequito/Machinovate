bf9e126891e2890ced581cc4d22f9053d8c5f385
Nyries <2013056563@ust-ics.mygbiz.com>
2016-02-12 14:57:04 +0800

    Reindent machines.php

--- web/machines.php
+++ web/machines.php
@@ -1,113 +1,113 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="styles/bootstrap.min.css">
-		<link rel="stylesheet" href="styles/main.css">
-		<title>Machinovate | Machines</title>
-		<style>
-			.footer{
-				margin-bottom:50px;
-				background: none;
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="styles/bootstrap.min.css">
+	<link rel="stylesheet" href="styles/main.css">
+	<title>Machinovate | Machines</title>
+	<style>
+	.footer{
+		margin-bottom:50px;
+		background: none;
 
-			}
-			.btn-primary{
-				margin-top:0%;
-				margin-left:auto;
-				margin-right:0%;
-			}
-			#images #image_1{
-				margin-top:-2%;
-				margin-left:250px;
-				margin-right:0px;
-				position: absolute;
-				
-			}
-			#images #image_2{
-				margin-top:-2%;
-				margin-left:575px;
-				margin-right:auto;
-				position: absolute;
-				display: inline-block;
-			}
-			#images #image_3{
-				margin-top:18%;
-				margin-left:23%;
-				margin-right: auto;
-				position: absolute;
-				display: inline-block;
-			}
-			#images #image_4{
-				margin-top:18%;
-				margin-left:56%;
-				margin-right: auto;
-				position: absolute;
-				display: inline-block;
-			}
-			
-			.btn-info{
-				margin-top:35%;
-				margin-left:-26%;
-				margin-right:auto;
-				position: absolute;
-				display: inline-block;
-				background-color: #132C55;
-				border-radius: 0;
-
-			}
-			.btn-default{
-				margin-top:-2%;
-				margin-left:45%;
-				margin-right:auto;
-				position: absolute;
-				display: inline-block;
-				background-color: #132C55;
-				border-radius: 0;
-				font-color:white;
-			}
-		</style>
-	</head>
-	<body>
-		<?php include 'header_before_login.php';?>
-		<div class="container-fluid">
-                <div class="row">
-	                <!-- Slitter-->
-			        <div class="col-sm-3 col-md-6">
-						<div id="image_1"class="col-sm-3 col-md-6">
-						<img src="images/slitter_front.png" style="width:310px;height:228px;">
-						  <a href="slitter.php" class="btn btn-info" role="button">Slitter</a>
-		    			</div>
-		    		</div>
-	                <!-- Sheeter -->
-			         <div class="col-sm-9 col-md-6">
-		    			<div id="image_2" class="col-sm-9 col-md-6">
-						<img src="images/sheeter-servo.png" style="width:304px;height:228px;">
-						<a href="sheeter.php" class="btn btn-info" role="button">Sheeter</a>
-		    			</div>
-		    		</div>	
-		    		<!-- Cutter -->
-		    		<div class="col-sm-3 col-md-6" >
-						<div id="image_3"class="col-sm-3 col-md-6" >
-						<img src="images/cutter.png" style="width:304px;height:228px;">
-						<a href="cutter.php" class="btn btn-info" role="button">Cutter</a>
-		    			</div>
-    			
-		    		</div>
-	                <!-- Other Products -->
-			         <div class="col-sm-9 col-md-6">
-		    			<div id="image_4" class="col-sm-9 col-md-6">
-						<img src="images/other.png" style="width:120px;height:178px;">
-						<a href="other.php" class="btn btn-info" role="button">Other Products</a>
-		    			</div>
-		    		</div>	
-                </div>
-		</div> <!-- /.container-fluid -->
+	}
+	.btn-primary{
+		margin-top:0%;
+		margin-left:auto;
+		margin-right:0%;
+	}
+	#images #image_1{
+		margin-top:-2%;
+		margin-left:250px;
+		margin-right:0px;
+		position: absolute;
 		
-		<script type="text/javascript" src="scripts/jquery-2.2.0.min.js"></script>
-		<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
-		<script type="text/javascript">
-			document.getElementById("machines").className = "active";
-		</script>
-	</body>
+	}
+	#images #image_2{
+		margin-top:-2%;
+		margin-left:575px;
+		margin-right:auto;
+		position: absolute;
+		display: inline-block;
+	}
+	#images #image_3{
+		margin-top:18%;
+		margin-left:23%;
+		margin-right: auto;
+		position: absolute;
+		display: inline-block;
+	}
+	#images #image_4{
+		margin-top:18%;
+		margin-left:56%;
+		margin-right: auto;
+		position: absolute;
+		display: inline-block;
+	}
+	
+	.btn-info{
+		margin-top:35%;
+		margin-left:-26%;
+		margin-right:auto;
+		position: absolute;
+		display: inline-block;
+		background-color: #132C55;
+		border-radius: 0;
+
+	}
+	.btn-default{
+		margin-top:-2%;
+		margin-left:45%;
+		margin-right:auto;
+		position: absolute;
+		display: inline-block;
+		background-color: #132C55;
+		border-radius: 0;
+		font-color:white;
+	}
+	</style>
+</head>
+<body>
+	<?php include 'header_before_login.php';?>
+	<div class="container-fluid">
+		<div class="row">
+			<!-- Slitter-->
+			<div class="col-sm-3 col-md-6">
+				<div id="image_1"class="col-sm-3 col-md-6">
+					<img src="images/slitter_front.png" style="width:310px;height:228px;">
+					<a href="slitter.php" class="btn btn-info" role="button">Slitter</a>
+				</div>
+			</div>
+			<!-- Sheeter -->
+			<div class="col-sm-9 col-md-6">
+				<div id="image_2" class="col-sm-9 col-md-6">
+					<img src="images/sheeter-servo.png" style="width:304px;height:228px;">
+					<a href="sheeter.php" class="btn btn-info" role="button">Sheeter</a>
+				</div>
+			</div>	
+			<!-- Cutter -->
+			<div class="col-sm-3 col-md-6" >
+				<div id="image_3"class="col-sm-3 col-md-6" >
+					<img src="images/cutter.png" style="width:304px;height:228px;">
+					<a href="cutter.php" class="btn btn-info" role="button">Cutter</a>
+				</div>
+				
+			</div>
+			<!-- Other Products -->
+			<div class="col-sm-9 col-md-6">
+				<div id="image_4" class="col-sm-9 col-md-6">
+					<img src="images/other.png" style="width:120px;height:178px;">
+					<a href="other.php" class="btn btn-info" role="button">Other Products</a>
+				</div>
+			</div>	
+		</div> <!-- /.row -->
+	</div> <!-- /.container-fluid -->
+	
+	<script type="text/javascript" src="scripts/jquery-2.2.0.min.js"></script>
+	<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
+	<script type="text/javascript">
+	document.getElementById("machines").className = "active";
+	</script>
+</body>
 </html>
