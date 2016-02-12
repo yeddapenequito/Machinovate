410d9bbee9e9f8c9ec063fdf731f064da3d189a9
Nyries <2013056563@ust-ics.mygbiz.com>
2016-02-12 10:11:56 +0800

    Modify contact_us.php

--- web/contact_us.php
+++ web/contact_us.php
@@ -12,26 +12,34 @@
 
 		<div id="contact-us-content" class="container">
 			<div class="row">
-				<div class="col-sm-12 col-md-8 col-lg-7">
+				<address class="col-sm-12 col-md-4">
+					<h2 class="page-header">Contact Information</h2>
+					<table class="table table-hover">
+						<tbody>
+							<tr>
+								<td>Address</td>
+								<td>Unit # 301-A King Center Bldg. Sgt Rivera St.Brgy. Manresa Quezon City Philippines 1115</td>
+							</tr>
+							<tr>
+								<td>Telephone</td>
+								<td>
+									<ul class="list-unstyled">
+										<li>+632 404 6676</li>
+										<li>+632 623 2988</li>
+										<li>+632 423 2988</li>
+									</ul>
+								</td>
+							</tr>
+							<tr>
+								<td>E-mail</td>
+								<td><a href="mailto:machinovate.international@gmail.com">machinovate.international@gmail.com</a></td>
+							</tr>
+						</tbody>
+					</table>
+				</address>
+				<div class="col-sm-12 col-md-8">
 					<iframe id="map" class="text-center" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.2098541594223!2d120.99441391418777!3d14.644025789774798!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b67b7a1ce8d1%3A0x5570c43b3bf78ccd!2s301%2C+King+Center+Bldg%2C+Sgt.+Rivera+St%2C+Quezon+City%2C+Metro+Manila!5e0!3m2!1sen!2sph!4v1454231280176" width="575" height="425" frameborder="0" style="border:0" allowfullscreen></iframe>
 				</div>
-				<address class="col-sm-12 col-md-4 col-lg-5">
-					<h1 class="page-header">Contact Information</h1>
-					<dl class="dl-horizontal">
-						<dt>Address</dt>
-						<dd>Unit # 301-A King Center Bldg. Sgt Rivera St.Brgy. Manresa Quezon City Philippines 1115</dd>
-						<dt>Telephone</dt>
-						<dd>
-							<ul class="list-unstyled">
-								<li>+632 404 6676</li>
-								<li>+632 623 2988</li>
-								<li>+632 423 2988</li>
-							</ul>
-						</dd>
-						<dt>E-mail</dt>
-						<dd><a href="mailto:machinovate.international@gmail.com">machinovate.international@gmail.com</a></dd>
-					</dl>
-				</address>
 			</div>
 		</div>	
 		
