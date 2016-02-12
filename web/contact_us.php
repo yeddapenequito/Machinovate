539abffb6b73647653a47ec0b9a0f60b426cfa76
Nyries <2013056563@ust-ics.mygbiz.com>
2016-02-12 09:53:57 +0800

    Rename id sitemap-content to contact-us-content
    So that it the same with the filename and tabname.

--- web/contact_us.php
+++ web/contact_us.php
@@ -5,12 +5,12 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="./styles/bootstrap.min.css">
 		<link rel="stylesheet" href="./styles/main.css">
-		<title>Machinovate | Sitemap</title>
+		<title>Machinovate | Contact Us</title>
 	</head>
 	<body>
 		<?php include 'header.php';?>
 
-		<div id="sitemap-content" class="container">
+		<div id="contact-us-content" class="container">
 			<div class="row">
 				<div class="col-sm-12 col-md-8 col-lg-7">
 					<iframe id="map" class="text-center" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.2098541594223!2d120.99441391418777!3d14.644025789774798!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b67b7a1ce8d1%3A0x5570c43b3bf78ccd!2s301%2C+King+Center+Bldg%2C+Sgt.+Rivera+St%2C+Quezon+City%2C+Metro+Manila!5e0!3m2!1sen!2sph!4v1454231280176" width="575" height="425" frameborder="0" style="border:0" allowfullscreen></iframe>
