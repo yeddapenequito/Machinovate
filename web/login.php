42ec5b48f3a1916cdc602c3d1f91404b2776725d
Nyries <2013056563@ust-ics.mygbiz.com>
2016-02-18 23:47:52 +0800

    Remove excess .row tag

--- web/login.php
+++ web/login.php
@@ -1,63 +1,60 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="styles/bootstrap.min.css">
-		<link rel="stylesheet" href="styles/main.css">
-		<title>Machinovate | Login</title>
-	</head>
-	<body>
-		<?php include 'header_before_login.php';?>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="styles/bootstrap.min.css">
+	<link rel="stylesheet" href="styles/main.css">
+	<title>Machinovate | Login</title>
+</head>
+<body>
+	<?php include 'header_before_login.php';?>
 
-		<div class="container">
-			<div class="row">
-				<div class="row">
-					<form role="form" action="form-handler" method="post" class="col-xs-offset-1 col-xs-10 col-sm-offset-3 col-sm-6 col-md-offset-4 col-md-4">
-						
-						<h3 class="page-header">Login</h3>
+	<div id="login-content" class="container">
+		<div class="row">
+			<form role="form" action="form-handler" method="post" class="col-xs-12 col-sm-offset-3 col-sm-6 col-md-offset-4 col-md-4">
 
-						<div class="form-group">
-							<label for="username">Username</label>
-							<input id="username" name="username" type="text" placeholder="Enter username" class="form-control" required autofocus> 
-						</div>
+				<h3 class="page-header">Login</h3>
 
-						<div class="form-group">
-							<label for="password">Password</label>
-							<input id="password" name="password" type="password" placeholder="Enter password" class="form-control" required> 
-						</div>
-						
-						<!-- The following code for implementing captcha is obtained and modified from http://www.the-art-of-web.com/php/captcha/-->
-
-						<div class="form-horizontal">
-							<div class="form-group">
-								<label for="captcha" class="control-label col-sm-5">
-									<img src="captcha.php" alt="CAPTCHA">
-								</label>
-								<div class="col-sm-7">
-									<input id="captcha" type="text" name="captcha" class="form-control">
-								</div>
-								<small>Copy the digits from the image into this box</small>
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="checkbox">
-							  <label for="remember"><input id="remember" type="checkbox" value="remember">Remember Me</label>
-							</div>
-						</div>
-
-						<div class="form-group">
-							<input id="login-button" type="submit" value="Login" class="btn btn-primary btn-block">
-						</div>
-
-						
-					</form>
+				<div class="form-group">
+					<label for="username">Username</label>
+					<input id="username" name="username" type="text" placeholder="Enter username" class="form-control" required autofocus> 
 				</div>
-			</div>
-		</div>
 
-		<script type="text/javascript" src="scripts/jquery-2.2.0.min.js"></script>
-		<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
-	</body>
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input id="password" name="password" type="password" placeholder="Enter password" class="form-control" required> 
+				</div>
+
+				<!-- The following code for implementing captcha is obtained and modified from http://www.the-art-of-web.com/php/captcha/-->
+
+				<div class="form-horizontal">
+					<div class="form-group">
+						<label for="captcha" class="control-label col-sm-5">
+							<img src="captcha.php" alt="CAPTCHA">
+						</label>
+						<div class="col-sm-7">
+							<input id="captcha" type="text" name="captcha" class="form-control">
+						</div>
+						<small>Copy the digits from the image into this box</small>
+					</div>
+				</div>
+
+				<div class="form-group">
+					<div class="checkbox">
+						<label for="remember"><input id="remember" type="checkbox" value="remember">Remember Me</label>
+					</div>
+				</div>
+
+				<div class="form-group">
+					<input id="login-button" type="submit" value="Login" class="btn btn-primary btn-block">
+				</div>
+
+			</form>
+		</div> <!-- /.row -->
+	</div>
+
+	<script type="text/javascript" src="scripts/jquery-2.2.0.min.js"></script>
+	<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
+</body>
 </html>
