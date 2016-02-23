e68c3aeaec4502c0b329aded9737f6ebb536370f
Nyries <2013056563@ust-ics.mygbiz.com>
2016-02-23 23:51:32 +0800

    Add content to machines.php

--- web/machines.php
+++ web/machines.php
@@ -6,87 +6,58 @@
 	<link rel="stylesheet" href="styles/bootstrap.min.css">
 	<link rel="stylesheet" href="styles/main.css">
 	<title>Machinovate | Machines</title>
-	<style>
-		#machines-content .mach-img {
-			width: 225px;
-			height: 150px;
-			display: block;
-			margin: 0 auto;
-		}
-		#machines-content header {
-			background-color: transparent;
-		}
-		#machines-content #categories > div{
-			border: 1px solid silver;
-			border-radius: 5px;
-			margin: 1em 0;
-		}
-		#machines-content #categories > div .bar {
-			background-color: silver;
-		}
-		#machines-content #categories .div-hover {
-			-webkit-transition: all .3s ease; /* Safari and Chrome */
-		  	-moz-transition: all .3s ease; /* Firefox */
-		  	-o-transition: all .3s ease; /* IE 9 */
-		  	-ms-transition: all .3s ease; /* Opera */
-		  	transition: all .3s ease;
-		}
-	</style>
 </head>
 <body>
 	<?php include 'header_before_login.php';?>
-	<div id="machines-content" class="container-fluid">
+	<div id="machines-content" class="container">
 		<div class="row">
-			<div id="categories" class="col-xs-12 col-sm-offset-2 col-sm-8">
-				<!-- Slitter-->
-				<div class="row div-hover">
-					<div class="col-xs-12 col-sm-8">
-						<header>
-							<h1>Slitter</h1>
-						</header>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla. Nunc sit amet erat sit amet justo aliquet vulputate non eu leo.</p>
-					</div>
-					<div class="col-xs-12 col-sm-4 bar">
-						<img class="mach-img" alt="slitter" src="images/slitter-front.png">
-					</div>
+			<!-- Cutter -->
+			<div class="col-xs-12 col-md-3">
+				<div class="minor">
+					<header>
+						<h1>Cutter</h1>
+					</header>
+					<img class="mach-img" alt="cutter" src="images/cutter.png">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla. Nunc sit amet erat sit amet justo aliquet vulputate non eu leo.</p>
+					<a href="" class="btn btn-primary learn-more-btn">Learn More&gt;&gt;</a>
 				</div>
-				<!-- Sheeter -->
-				<div class="row div-hover">
-					<div class="col-xs-12 col-sm-8">
-						<header>
-							<h1>Sheeter</h1>
-						</header>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla. Nunc sit amet erat sit amet justo aliquet vulputate non eu leo.</p>
-					</div>
-					<div class="col-xs-12 col-sm-4 bar">
-						<img class="mach-img" alt="sheeter" src="images/sheeter-servo.png">
-					</div>
+			</div>
+
+			<!-- Slitter-->
+			<div class="col-xs-12 col-md-3">
+				<div>
+					<header>
+						<h1>Slitter</h1>
+					</header>
+					<img class="mach-img" alt="slitter" src="images/slitter-front.png">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla. Nunc sit amet erat sit amet justo aliquet vulputate non eu leo.</p>
+					<a href="" class="btn btn-primary learn-more-btn">Learn More&gt;&gt;</a>
 				</div>
-				<!-- Cutter -->
-				<div class="row div-hover">
-					<div class="col-xs-12 col-sm-8">
-						<header>
-							<h1>Cutter</h1>
-						</header>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla. Nunc sit amet erat sit amet justo aliquet vulputate non eu leo.</p>
-					</div>
-					<div class="col-xs-12 col-sm-4 bar">
-						<img class="mach-img" alt="cutter" src="images/cutter.png">
-					</div>
+			</div>
+
+			<!-- Sheeter -->
+			<div class="col-xs-12 col-md-3">
+				<div>
+					<header>
+						<h1>Sheeter</h1>
+					</header>
+					<img class="mach-img" alt="sheeter" src="images/sheeter-servo.png">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla. Nunc sit amet erat sit amet justo aliquet vulputate non eu leo.</p>
+					<a href="" class="btn btn-primary learn-more-btn">Learn More&gt;&gt;</a>
 				</div>
-				<!-- Other Products -->
-				<div class="row div-hover">
-					<div class="col-xs-12 col-sm-8">
-						<header>
-							<h1>Other Products</h1>
-						</header>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla. Nunc sit amet erat sit amet justo aliquet vulputate non eu leo.</p>
-					</div>
-					<div class="col-xs-12 col-sm-4 bar">
-						<img class="mach-img" alt="other-products" src="images/other-products.png">
-					</div>
+			</div>
+
+			<!-- Other Products -->
+			<div class="col-xs-12 col-md-3">
+				<div class="minor">
+					<header>
+						<h1>Other Products</h1>
+					</header>
+					<img class="mach-img" alt="other-products" src="images/other-products.png">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla. Nunc sit amet erat sit amet justo aliquet vulputate non eu leo.</p>
+					<a href="" class="btn btn-primary learn-more-btn">Learn More&gt;&gt;</a>
 				</div>
-			</div>	
+			</div>
 		</div> <!-- /.row -->
 	</div> <!-- /.container-fluid -->
 	
