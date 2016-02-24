54b5a1c87bd97dd114a496731ee5aa309b3611c6
yeddapenequito <2013056537@ust-ics.mygbiz.com>
2016-02-24 17:23:22 +0800

    Create Agents page

--- web/agents.php
+++ web/agents.php
@@ -4,11 +4,97 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="styles/bootstrap.min.css">
+		<link rel="stylesheet" href="styles/flag-icon-css-master/css/flag-icon.css">
+		<link rel="stylesheet" href="styles/flag-icon-css-master/css/flag-icon.min.css">
 		<link rel="stylesheet" href="styles/main.css">
 		<title>Machinovate | Agents</title>
 	</head>
 	<body>
 		<?php include 'header_before_login.php';?>
+
+		<div class="container-fluid">
+		
+		  <div class="row">
+		    <div class="col-xs-12 col-md-offset-3 col-md-6" style="background-color:lavender;">
+		      <!--for other way of src(/webb/styles/flag-icon-css-master/flags/4x3/ph.svg)-->
+		      <img class="col-xs-4" src="../flags/4x3/ph.svg"><p class="col-xs-8"><b><u>PHILIPPINES</u></b></p>
+		      <p class="col-xs-8"><b>Name: </b>  <a>LASTNAME, FirstName M.I</a></p> 
+		      <p class="col-xs-8"><b>Email Address:</b> <a>somename@somename.com </a></p>
+		      <p class="col-xs-8"><b>Moblie No: </b> <a>+000 0000 0000</a></p>
+		</div>
+
+		    
+  </div>
+
+</div>
+<br>
+<div class="container-fluid">
+		  
+		  <div class="row">
+		    <div class="col-xs-12 col-md-offset-3 col-md-6" style="background-color:lavender;">
+		    
+		      <img class="col-xs-4" src="../flags/4x3/cn.svg"><p class="col-xs-8"><b><u>CHINA</u></b></p>
+		      <p class="col-xs-8"><b>Name: </b>  <a>LASTNAME, FirstName M.I</a></p> 
+		      <p class="col-xs-8"><b>Email Address:</b> <a>somename@somename.com </a></p>
+		      <p class="col-xs-8"><b>Moblie No: </b> <a>+000 0000 0000</a></p>
+		</div>
+
+		    
+  </div>
+  
+</div>
+<br>
+<div class="container-fluid">
+		  
+		  <div class="row">
+		    <div class="col-xs-12 col-md-offset-3 col-md-6" style="background-color:lavender;">
+		     
+		      <img class="col-xs-4" src="../flags/4x3/th.svg"><b><u>THAILAND</u></b></p>
+		      <p class="col-xs-8"><b>Name: </b>  <a>LASTNAME, FirstName M.I</a></p> 
+		      <p class="col-xs-8"><b>Email Address:</b> <a>somename@somename.com </a></p>
+		      <p class="col-xs-8"><b>Moblie No: </b> <a>+000 0000 0000</a></p>
+		</div>
+
+		    
+  </div>
+  
+</div>
+<br>
+<div class="container-fluid">
+		  
+		  <div class="row">
+		    <div class="col-xs-12 col-md-offset-3 col-md-6" style="background-color:lavender;">
+		     
+		      <img class="col-xs-4" src="../flags/4x3/in.svg"><b><u>INDIA</u></b></p>
+		      <p class="col-xs-8"><b>Name: </b>  <a>LASTNAME, FirstName M.I</a></p> 
+		      <p class="col-xs-8"><b>Email Address:</b> <a>somename@somename.com </a></p>
+		      <p class="col-xs-8"><b>Moblie No: </b> <a>+000 0000 0000</a></p>
+		</div>
+
+		    
+  </div>
+  
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 		<script type="text/javascript" src="scripts/jquery-2.2.0.min.js"></script>
 		<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
