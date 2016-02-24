79b2ccf7eb4e09d11555c7906c853bea3e27b38a
Nyries <2013056563@ust-ics.mygbiz.com>
2016-02-24 23:20:16 +0800

    Use correct jquery version

--- web/slitter.php
+++ web/slitter.php
@@ -164,7 +164,7 @@
 			</div> <!-- /.row -->
 		</div> <!-- /.container -->
 
-		<script type="text/javascript" src="scripts/jquery-2.1.4.min.js"></script>
+		<script type="text/javascript" src="scripts/jquery-2.2.0.min.js"></script>
 		<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
 		<script type="text/javascript">
 			$(".img-popup").imageBox();
