d122fb53636851eaf77448ecb8dc72bba81edacd
Nyries <2013056563@ust-ics.mygbiz.com>
2016-02-25 09:32:59 +0800

    Add styles to agents.php

--- web/agents.php
+++ web/agents.php
@@ -4,7 +4,6 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="styles/bootstrap.min.css">
-		<link rel="stylesheet" href="styles/flag-icon-css-master/css/flag-icon.css">
 		<link rel="stylesheet" href="styles/flag-icon-css-master/css/flag-icon.min.css">
 		<link rel="stylesheet" href="styles/main.css">
 		<title>Machinovate | Agents</title>
@@ -12,94 +11,61 @@
 	<body>
 		<?php include 'header_before_login.php';?>
 
-		<div class="container-fluid">
-		
-		  <div class="row">
-		    <div class="col-xs-12 col-md-offset-3 col-md-6" style="background-color:lavender;">
-		      <!--for other way of src(/webb/styles/flag-icon-css-master/flags/4x3/ph.svg)-->
-		      <img class="col-xs-4" src="../flags/4x3/ph.svg"><p class="col-xs-8"><b><u>PHILIPPINES</u></b></p>
-		      <p class="col-xs-8"><b>Name: </b>  <a>LASTNAME, FirstName M.I</a></p> 
-		      <p class="col-xs-8"><b>Email Address:</b> <a>somename@somename.com </a></p>
-		      <p class="col-xs-8"><b>Moblie No: </b> <a>+000 0000 0000</a></p>
+		<div id="agents-content" class="container-fluid">
+			<div class="row">
+				<div class="col-xs-12 col-md-offset-3 col-md-6 agent">
+					<img class="col-xs-4" src="styles/flag-icon-css-master/flags/4x3/ph.svg">
+					<div class="col-xs-8">
+						<p><b><u>PHILIPPINES</u></b></p>
+						<p><b>Name: </b>  <a>LASTNAME, FirstName M.I</a></p>
+						<p><b>Email Address:</b> <a>somename@somename.com </a></p>
+						<p><b>Mobile No: </b> <a>+000 0000 0000</a></p>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="row">
+				<div class="col-xs-12 col-md-offset-3 col-md-6 agent">
+					<img class="col-xs-4" src="styles/flag-icon-css-master/flags/4x3/cn.svg">
+					<div class="col-xs-8">
+						<p><b><u>CHINA</u></b></p>
+						<p><b>Name: </b>  <a>LASTNAME, FirstName M.I</a></p>
+						<p><b>Email Address:</b> <a>somename@somename.com </a></p>
+						<p><b>Mobile No: </b> <a>+000 0000 0000</a></p>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-xs-12 col-md-offset-3 col-md-6 agent">
+					<img class="col-xs-4" src="styles/flag-icon-css-master/flags/4x3/th.svg">
+					<div class="col-xs-8">
+						<p><b><u>THAILAND</u></b></p>
+						<p><b>Name: </b>  <a>LASTNAME, FirstName M.I</a></p>
+						<p><b>Email Address:</b> <a>somename@somename.com </a></p>
+						<p><b>Mobile No: </b> <a>+000 0000 0000</a></p>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-xs-12 col-md-offset-3 col-md-6 agent">
+					<img class="col-xs-4" src="styles/flag-icon-css-master/flags/4x3/in.svg">
+					<div class="col-xs-8">
+						<p><b><u>INDIA</u></b></p>
+						<p><b>Name: </b>  <a>LASTNAME, FirstName M.I</a></p>
+						<p><b>Email Address:</b> <a>somename@somename.com </a></p>
+						<p><b>Mobile No: </b> <a>+000 0000 0000</a></p>
+					</div>
+				</div>
+			</div>
 		</div>
-
-		    
-  </div>
-
-</div>
-<br>
-<div class="container-fluid">
-		  
-		  <div class="row">
-		    <div class="col-xs-12 col-md-offset-3 col-md-6" style="background-color:lavender;">
-		    
-		      <img class="col-xs-4" src="../flags/4x3/cn.svg"><p class="col-xs-8"><b><u>CHINA</u></b></p>
-		      <p class="col-xs-8"><b>Name: </b>  <a>LASTNAME, FirstName M.I</a></p> 
-		      <p class="col-xs-8"><b>Email Address:</b> <a>somename@somename.com </a></p>
-		      <p class="col-xs-8"><b>Moblie No: </b> <a>+000 0000 0000</a></p>
-		</div>
-
-		    
-  </div>
-  
-</div>
-<br>
-<div class="container-fluid">
-		  
-		  <div class="row">
-		    <div class="col-xs-12 col-md-offset-3 col-md-6" style="background-color:lavender;">
-		     
-		      <img class="col-xs-4" src="../flags/4x3/th.svg"><b><u>THAILAND</u></b></p>
-		      <p class="col-xs-8"><b>Name: </b>  <a>LASTNAME, FirstName M.I</a></p> 
-		      <p class="col-xs-8"><b>Email Address:</b> <a>somename@somename.com </a></p>
-		      <p class="col-xs-8"><b>Moblie No: </b> <a>+000 0000 0000</a></p>
-		</div>
-
-		    
-  </div>
-  
-</div>
-<br>
-<div class="container-fluid">
-		  
-		  <div class="row">
-		    <div class="col-xs-12 col-md-offset-3 col-md-6" style="background-color:lavender;">
-		     
-		      <img class="col-xs-4" src="../flags/4x3/in.svg"><b><u>INDIA</u></b></p>
-		      <p class="col-xs-8"><b>Name: </b>  <a>LASTNAME, FirstName M.I</a></p> 
-		      <p class="col-xs-8"><b>Email Address:</b> <a>somename@somename.com </a></p>
-		      <p class="col-xs-8"><b>Moblie No: </b> <a>+000 0000 0000</a></p>
-		</div>
-
-		    
-  </div>
-  
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 		<script type="text/javascript" src="scripts/jquery-2.2.0.min.js"></script>
 		<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
 		<script type="text/javascript">
-			document.getElementById("agents").className = "active";
+		document.getElementById("agents").className = "active";
 		</script>
 	</body>
 </html>
