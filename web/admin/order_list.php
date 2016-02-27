b0023e98a10fff20d74c762a78e8aadfdf82334c
Nyries <2013056563@ust-ics.mygbiz.com>
2016-02-27 21:44:57 +0800

    Add bootstrap tabs on order_list.php

--- web/admin/order_list.php
+++ web/admin/order_list.php
@@ -9,6 +9,39 @@
 </head>
 <body>
 	<?php include 'header_after_login.php';?>
+	
+	<div class="container">
+		<ul class="nav nav-tabs">
+		  <li><a data-toggle="tab" href="#menu1">All</a></li>
+		  <li class="active"><a data-toggle="tab" href="#home">Pending</a></li>
+		  <li><a data-toggle="tab" href="#menu2">Cancelled</a></li>
+		  <li><a data-toggle="tab" href="#menu3">Confirmed</a></li>
+		  <li><a data-toggle="tab" href="#menu4">Completed</a></li>
+		</ul>
+
+		<div class="tab-content">
+		  <div id="menu1" class="tab-pane fade">
+		    <h3>Menu 1</h3>
+		    <p>Some content in menu 1.</p>
+		  </div>
+		  <div id="home" class="tab-pane fade in active">
+		    <h3>HOME</h3>
+		    <p>Some content.</p>
+		  </div>
+		  <div id="menu2" class="tab-pane fade">
+		    <h3>Menu 2</h3>
+		    <p>Some content in menu 2.</p>
+		  </div>
+		  <div id="menu3" class="tab-pane fade">
+		    <h3>Menu 3</h3>
+		    <p>Some content in menu 3.</p>
+		  </div>
+		  <div id="menu4" class="tab-pane fade">
+		    <h3>Menu 4</h3>
+		    <p>Some content in menu 4.</p>
+		  </div>
+		</div> <!-- /.tab-content -->
+	</div> <!-- /.container -->
 	<script type="text/javascript" src="../scripts/jquery-2.2.0.min.js"></script>
 	<script type="text/javascript" src="../scripts/bootstrap.min.js"></script>
 	<script type="text/javascript">
