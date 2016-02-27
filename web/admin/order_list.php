2a05fdd7f0ccc4a784b8735cb73729ed8fcead0b
Nyries <2013056563@ust-ics.mygbiz.com>
2016-02-27 08:47:13 +0800

    Create generate_reports and order_list page

---
+++ web/admin/order_list.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="../styles/bootstrap.min.css">
+	<link rel="stylesheet" href="../styles/main.css">
+	<title>Machinovate | Order List</title>
+</head>
+<body>
+	<?php include 'header_after_login.php';?>
+	<script type="text/javascript" src="../scripts/jquery-2.2.0.min.js"></script>
+	<script type="text/javascript" src="../scripts/bootstrap.min.js"></script>
+	<script type="text/javascript">
+		document.getElementById("order-list").className = "active";
+	</script>
+</body>
+</html>
