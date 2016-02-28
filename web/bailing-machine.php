f9ffed66d1bb989f704589715a9daec39b5aa132
yeddapenequito <2013056537@ust-ics.mygbiz.com>
2016-02-28 13:00:13 +0800

    Add modal functionality to images but not yet centered

--- web/bailing-machine.php
+++ web/bailing-machine.php
@@ -7,6 +7,95 @@
 		<link rel="stylesheet" href="styles/flag-icon-css-master/css/flag-icon.min.css">
 		<link rel="stylesheet" href="styles/main.css">
 		<title>Machinovate | Bailing Machine</title>
+		<style>
+			#myImg {
+			    border-radius:0px;
+			    cursor: pointer;
+			    transition: 0.3s;
+
+			}
+
+			#myImg:hover {opacity: 0.7;}
+
+			/* The Modal (background) */
+			.modal {
+			    display: none; /* Hidden by default */
+			    position: fixed; /* Stay in place */
+			    z-index: 1; /* Sit on top */
+			    padding-top: 100px; /* Location of the box */
+			    left: 0;
+			    top: 0;
+			    width: 100%; /* Full width */
+			    height: 100%; /* Full height */
+			    overflow: auto; /* Enable scroll if needed */
+			    background-color: rgb(0,0,0); /* Fallback color */
+			    background-color: rgba(0,0,0,0.9); /* Black w/ opacity */
+			}
+
+			/* Modal Content (image) */
+			.modal-content {
+			    margin: auto;
+			    display: block;
+			    width: 80%;
+			    max-width: 700px;
+			    align-content: center;
+			}
+
+			/* Caption of Modal Image */
+			#caption {
+			    margin: auto;
+			    display: block;
+			    width: 80%;
+			    max-width: 700px;
+			    text-align: center;
+			    color: #ccc;
+			    padding: 10px 0;
+			    height: 150px;
+			}
+
+			/* Add Animation */
+			.modal-content, #caption {    
+			    -webkit-animation-name: zoom;
+			    -webkit-animation-duration: 0.6s;
+			    animation-name: zoom;
+			    animation-duration: 0.6s;
+			}
+
+			@-webkit-keyframes zoom {
+			    from {transform:scale(0)} 
+			    to {transform:scale(1)}
+			}
+
+			@keyframes zoom {
+			    from {transform:scale(0)} 
+			    to {transform:scale(1)}
+			}
+
+			/* The Close Button */
+			.close {
+			    position: absolute;
+			    top: 15px;
+			    right: 35px;
+			    color: #f1f1f1;
+			    font-size: 40px;
+			    font-weight: bold;
+			    transition: 0.3s;
+			}
+
+			.close:hover,
+			.close:focus {
+			    color: #bbb;
+			    text-decoration: none;
+			    cursor: pointer;
+			}
+
+			/* 100% Image Width on Smaller Screens */
+			@media only screen and (max-width: 700px){
+			    .modal-content {
+			        width: 100%;
+			    }
+			}
+</style>
 	</head>
 	<body>
 		<?php include 'header_before_login.php';?>
@@ -83,10 +172,13 @@
 					<div id="photos">
 						<h3 class="page-header">Photos</h3>
 						<div class="img-popup">
-							<img alt="slitter front" src="images/slitter-front.png"></img>
-							<img alt="slitter back" src="images/slitter-back.png"></img>
-							<img alt="slitter close up 1" src="images/slitter-close-up-1.jpg"></img>
-							<img alt="slitter close up 2" src="images/slitter-close-up-2.jpg"></img>
+							<img id="myImg" src="images/bailing-machine.png" alt="bailing-machine">
+						</div>
+						<!-- The Modal -->
+						<div id="myModal" class="modal">
+						  <span class="close">×</span>
+						  <img class="modal-content" id="bailing-machine">
+						  <div id="caption"></div>
 						</div>
 					</div>
 
@@ -106,5 +198,28 @@
 
 		<script type="text/javascript" src="scripts/jquery-2.2.0.min.js"></script>
 		<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
+		<script>
+			// Get the modal
+			var modal = document.getElementById('myModal');
+
+			// Get the image and insert it inside the modal - use its "alt" text as a caption
+			var img = document.getElementById('myImg');
+			var modalImg = document.getElementById("bailing-machine");
+			var captionText = document.getElementById("caption");
+			img.onclick = function(){
+			    modal.style.display = "block";
+			    modalImg.src = this.src;
+			    modalImg.alt = this.alt;
+			    captionText.innerHTML = this.alt;
+			}
+
+			// Get the <span> element that closes the modal
+			var span = document.getElementsByClassName("close")[0];
+
+			// When the user clicks on <span> (x), close the modal
+			span.onclick = function() { 
+			    modal.style.display = "none";
+			}
+			</script>
 	</body>
 </html>
