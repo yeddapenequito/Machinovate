6d16a93d880ba4ea850e0913aa30d3d129ddc1f7
yeddapenequito <2013056537@ust-ics.mygbiz.com>
2016-02-28 22:54:09 +0800

    Remove (comment) order now in navbar in home

--- web/header_before_login_home_only.php
+++ web/header_before_login_home_only.php
@@ -22,7 +22,7 @@
 			<ul class="nav navbar-nav">
 				<li><a id="home" href="index.php">Home</a></li>
 				<li><a id="machines" href="machines.php">Machines</a></li>
-				<li><a id="order-now" href="order_now.php">Order Now</a></li>
+				<!-- <li><a id="order-now" href="order_now.php">Order Now</a></li> -->
 				<li><a id="events" href="events.php">Events</a></li>
 				<li><a id="contact-us" href="contact_us.php">Contact Us</a></li>
 				<li><a id="agents" href="agents.php">Agents</a></li>
