62ec8896681a209d78c0838ed41944d57f14851c
natsukihanamura <2013056531@ust-ics.mygbiz.com>
2016-02-28 14:27:14 +0800

    --edited some typos in agents.php

--- web/agents.php
+++ web/agents.php
@@ -13,57 +13,6 @@
 	<body>
 		<?php include 'header_before_login.php';?>
 
-		<div id="agents-content" class="container-fluid">
-			<div class="row">
-				<div class="col-xs-12 col-md-offset-3 col-md-6 agent">
-					<img class="col-xs-4" src="styles/flag-icon-css-master/flags/4x3/ph.svg">
-					<div class="col-xs-8">
-						<p><b><u>PHILIPPINES</u></b></p>
-						<p><b>Name: </b>  <a>LASTNAME, FirstName M.I</a></p>
-						<p><b>Email Address:</b> <a>somename@somename.com </a></p>
-						<p><b>Mobile No: </b> <a>+000 0000 0000</a></p>
-					</div>
-				</div>
-			</div>
-
-
-			<div class="row">
-				<div class="col-xs-12 col-md-offset-3 col-md-6 agent">
-					<img class="col-xs-4" src="styles/flag-icon-css-master/flags/4x3/cn.svg">
-					<div class="col-xs-8">
-						<p><b><u>CHINA</u></b></p>
-						<p><b>Name: </b>  <a>LASTNAME, FirstName M.I</a></p>
-						<p><b>Email Address:</b> <a>somename@somename.com </a></p>
-						<p><b>Mobile No: </b> <a>+000 0000 0000</a></p>
-					</div>
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="col-xs-12 col-md-offset-3 col-md-6 agent">
-					<img class="col-xs-4" src="styles/flag-icon-css-master/flags/4x3/th.svg">
-					<div class="col-xs-8">
-						<p><b><u>THAILAND</u></b></p>
-						<p><b>Name: </b>  <a>LASTNAME, FirstName M.I</a></p>
-						<p><b>Email Address:</b> <a>somename@somename.com </a></p>
-						<p><b>Mobile No: </b> <a>+000 0000 0000</a></p>
-					</div>
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="col-xs-12 col-md-offset-3 col-md-6 agent">
-					<img class="col-xs-4" src="styles/flag-icon-css-master/flags/4x3/in.svg">
-					<div class="col-xs-8">
-						<p><b><u>INDIA</u></b></p>
-						<p><b>Name: </b>  <a>LASTNAME, FirstName M.I</a></p>
-						<p><b>Email Address:</b> <a>somename@somename.com </a></p>
-						<p><b>Mobile No: </b> <a>+000 0000 0000</a></p>
-					</div>
-				</div>
-			</div>
-		</div>
-
 		<script type="text/javascript" src="scripts/jquery-2.2.0.min.js"></script>
 		<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
 		<script type="text/javascript">
@@ -81,7 +30,7 @@ $page_title = 'View the Agent';
 
 
 // Page header:
-echo '<h1>Registered Users</h1>';
+echo '<h1>Agents</h1>';
 
 require ('../mysqli_connect.php'); // Connect to the db.
 		
