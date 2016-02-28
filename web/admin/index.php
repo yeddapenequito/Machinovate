f99e771666486568c8896a164508c24c986fc7fd
Nyries <2013056563@ust-ics.mygbiz.com>
2016-02-28 12:38:39 +0800

    Invert colors on admin side login page

--- web/admin/index.php
+++ web/admin/index.php
@@ -5,6 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="../styles/bootstrap.min.css">
 	<link rel="stylesheet" href="../styles/main.css">
+	<link rel="stylesheet" href="../styles/admin.css">
 	<title>Machinovate | Login</title>
 </head>
 <body>
