3ab60ff90b2331966c422765ef7c54d35d6d6659
yeddapenequito <2013056537@ust-ics.mygbiz.com>
2016-02-29 23:23:08 +0800

    Add temporary homepage-image

--- web/index.php
+++ web/index.php
@@ -6,11 +6,25 @@
 		<link rel="stylesheet" href="styles/bootstrap.min.css">
 		<link rel="stylesheet" href="styles/main.css">
 		<title>Machinovate | Home</title>
+			<style>
+			img { 
+			    width:100%; 
+			    margin-left:0px;
+			    
+			}
+			.container-fluid{
+			background-color: #153a7e;}
+			</style>
 	</head>
 	<body>
 		<?php include 'header_before_login_home_only.php';?>
 		
 		<!--insert slideshow or carousel code here -->
+		<!-- Tentative Image for homepage -->
+		<div class= "container-fluid">
+				<img src="images/Tentative_homepage.png" alt="homepage">
+		</div>
+	
 		
 		<script type="text/javascript" src="scripts/jquery-2.2.0.min.js"></script>
 		<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
