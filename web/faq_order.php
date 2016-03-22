c7f662b7210b876e1bf2892e6ac849be9ee37680
yeddapenequito <2013056537@ust-ics.mygbiz.com>
2016-03-22 19:09:55 +0800

    Add order cutter page (header is still not place)

---
+++ web/faq_order.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="./styles/bootstrap.min.css">
+		<link rel="stylesheet" href="./styles/main.css">
+		<title>Machinovate | FAQ</title>
+	</head>
+	<body>
+		<?php include 'header_before_login.php';?>
+
+		  <!-- Order Section -->
+    <section class="success" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>How to Order?</h2>
+                    <hr>
+                    
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-2">
+                    <p>You can order by going to <b>Machines</b> page > Choose among <b>Sheeter/ Slitter/ Cutter / Other Products</b> > Choose you desired product. > Press <b>Add to Cart</b> button</p>
+                </div>
+                
+                </div>
+            </div>
+        </div>
+    </section>
+      <!-- Specification Section -->
+    <section class="success" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Specification of Machines</h2>
+                    <hr>
+                    
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-2">
+                    <p>The Specifications of Machines can be found in the Machines page. The Specifications are specified according to its product. You can look the specifications of your preferred machine type by going to<b>Machines</b> page > Choose among <b>Sheeter/ Slitter/ Cutter / Other Products</b> > Choose you desired product.</p>
+                </div>
+                
+                </div>
+            </div>
+        </div>
+    </section>
+
+      <!-- How can you contact us? -->
+    <section class="success" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>How can you contact us?</h2>
+                    <hr>
+                    
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-2">
+                    <p>You can contact us by going to Contact Us page, and contact details are specified.</p>
+                </div>
+              
+             
+                </div>
+            </div>
+        </div>
+    </section>
+		
+		<script type="text/javascript" src="./scripts/jquery-2.2.0.min.js"></script>
+		<script type="text/javascript" src="./scripts/bootstrap.min.js"></script>
+		<script type="text/javascript">
+			document.getElementById("").className = "active";
+		</script>
+	</body>
+</html>
