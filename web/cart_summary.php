68571d1b022eaeea2128eb0cfa36c78ad13b851d
Marius Sanchez <2013056549@ust-ics.mygbiz.com>
2016-03-23 10:59:44 +0800

    order. di pa tapos

---
+++ web/cart_summary.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<script type="text/javascript" src="http://localhost/pop-up/jquery/jquery.js"></script>
+<script type="text/javascript" src="http://localhost/pop-up/popup-window.js"></script>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="styles/bootstrap.min.css">
+		<link rel="stylesheet" href="styles/main.css">
+		<title>Machinovate | Email</title>
+	</head>
+	<body>
+		<?php include 'header_before_login.php';?>
+
+		<div class="container">
+			<div class="form-group">
+					<div class="col-sm-offset-2 col-sm-10">
+						<legend>Hello from the Other Side</legend>
+						<?php
+						session_start();
+						
+						echo '<a href="order_slitter.php">asdf</a>';
+						?>
+						<!-- Anchor start -->
+<a href="#" onclick="popup_window_show('#popup_window_id_C5CEEF623548725D289E0986CEF38D9F', { pos : 'window-center', parent : this, x : 0, y : 0, width : 'auto' }); return false;">Open popup window</a>
+<!-- Anchor end -->
+
+<!-- Popup Window start -->
+<div id="popup_window_id_C5CEEF623548725D289E0986CEF38D9F" class="popup_window_css"><table class="popup_window_css"><tr><td class="popup_window_css"><div class="popup_window_css_head"><img src="http://localhost/pop-up/images/close.gif" alt="" width="9" height="9" />Customized Machine Specification Request: Slitter</div><div class="popup_window_css_body"><p>GENERAL SECTION
+<p>Model Name: </p>
+<p>Production Volume: </p>
+<p>Paper Type: </p>
+<p>GSM: </p>
+<p>Roll Diameter: </p>
+<p>Slitting Width: </p>
+</br>
+<p>CUTTING SECTION </p>
+<p>Standard [Five (5)] Set Male / Female Circular Blade with Trim Removal Blower</p>
+<p>    Additional Set of One Pair Male/Female: </p>
+<p>Core Cutter Machine: </p>
+</br>
+<p>UNWIND ROLL STAND SECTION </p>
+<p>Reel Stand:</p>
+<p>    Hydraulic Shaftless with Air Brake: </p>
+<p>    Single:  </p>
+<p>    Customized: </p>
+<p>    Stationary Shaft Stand: </p>
+<p>    Others: </p>
+<p>Ton Capacity: </p>
+</br>
+<p>SYSTEMS SECTION </p>
+<p>    Heavy Duty Web Guide Hydraulic EPC Control System with Automation – Centring Sensor and Control, 4 pieces Roller and Roll Steering Guiding with Base: </p>
+<p>    Heavy Duty Tension Control System with Auto Tension Controller, Auto Tension Sensor, Air Pressure Transducer and Tension Roller with Bearing Housing: </p>
+<p>    Banana Roll and Tension Roller: </p>
+<p>    Water Cooling Brake System: </p>
+<p>    Fully Computerized Control System: </p>
+<p>    Others: </p>
+</br>
+<p>OTHER DETAILS </p>
+</br></div><div class="popup_window_css_foot"><a href="http://www.php-development.ru/javascripts/popup-window.php" title="Powered by PHPDevel Popup Window | PHPDevel web scripts collection"><img src="http://localhost/pop-up/images/about.gif" alt="" width="6" height="6" /></a></div></td></tr></table></div>
+<!-- Popup Window end -->
+					</div>
+				</div>
+		</div> <!-- /.container -->
+
+			<script type="text/javascript" src="scripts/jquery-2.2.0.min.js"></script>
+		<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
+	</body>
+</html>
