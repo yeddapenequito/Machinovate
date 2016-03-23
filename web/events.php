a1d9f7070e59e83e9a8b3423a20f17d3d1b762e2
natsukihanamura <2013056531@ust-ics.mygbiz.com>
2016-03-23 22:36:10 +0800

    --done with events.php :) paayos n lng ng front end <3

--- web/events.php
+++ web/events.php
@@ -10,123 +10,7 @@
 <body>
 	<?php include 'header_before_login.php';?>
 
-	<div id="event-content" class="container">
-		<div class="row">
-			<ul class="nav nav-pills nav-stacked col-xs-12 col-sm-4">
-				<li class="active">
-					<a data-toggle="pill" href="#menu0">
-						<p id="event-name">2015 - Paperex India <span class="label label-success">Coming Soon</span></p>
-						<p>Pragati, Maidan, New Delhi India</p>
-						<p>Friday, February 12 </p>
-					</a>
-				</li>
-				<li><a data-toggle="pill" href="#menu1">2014 - Event Name</a></li>
-				<li><a data-toggle="pill" href="#menu2">2013 - Event Name</a></li>
-				<li><a data-toggle="pill" href="#menu3">2012 - Event Name</a></li>
-				<li><a data-toggle="pill" href="#menu4">2011 - Event Name</a></li>
-				<li><a data-toggle="pill" href="#menu5">2010 - Event Name</a></li>					
-			</ul> <!-- /.nav -->
-
-			<div class="tab-content col-xs-12 col-sm-8">
-				<div id="menu0" class="tab-pane fade in active">
-					<div class="page-header">
-						<h3>2015 - Paperex India</h3>
-					</div>
-					<figure>
-						<img src="images/paperex-india-event-1.jpg" alt="event picture">
-						<figcaption>Fig1. - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla.</figcaption>
-					</figure>
-					<figure>
-						<img src="images/paperex-india-event-2.jpg" alt="event picture">
-						<figcaption>Fig2. - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla.</figcaption>
-					</figure>
-					<figure>
-						<img src="images/paperex-india-event-3.jpg" alt="event picture">
-						<figcaption>Fig3. - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla.</figcaption>
-					</figure>
-				</div>
-				<div id="menu1" class="tab-pane fade">
-					<div class="page-header">
-						<h3>2014 - Event Name</h3>
-					</div>
-					<figure>
-						<img src="images/paperex-india-event-1.jpg" alt="event picture">
-						<figcaption>Fig1. - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla.</figcaption>
-					</figure>
-					<figure>
-						<img src="images/paperex-india-event-2.jpg" alt="event picture">
-						<figcaption>Fig2. - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla.</figcaption>
-					</figure>
-					<figure>
-						<img src="images/paperex-india-event-3.jpg" alt="event picture">
-						<figcaption>Fig3. - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla.</figcaption>
-					</figure>
-				</div>
-				<div id="menu2" class="tab-pane fade">
-					<div class="page-header">
-						<h3>2013 - Event Name</h3>
-					</div>
-					<figure>
-						<img src="images/paperex-india-event-1.jpg" alt="event picture">
-						<figcaption>Fig1. - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla.</figcaption>
-					</figure>
-					<figure>
-						<img src="images/paperex-india-event-2.jpg" alt="event picture">
-						<figcaption>Fig2. - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla.</figcaption>
-					</figure>
-					<figure>
-						<img src="images/paperex-india-event-3.jpg" alt="event picture">
-						<figcaption>Fig3. - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla.</figcaption>
-					</figure>
-				</div>
-				<div id="menu3" class="tab-pane fade">
-					<div class="page-header">
-						<h3>2012 - Event Name</h3>
-					</div>
-					<figure>
-						<img src="images/paperex-india-event-1.jpg" alt="event picture">
-						<figcaption>Fig1. - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla.</figcaption>
-					</figure>
-					<figure>
-						<img src="images/paperex-india-event-2.jpg" alt="event picture">
-						<figcaption>Fig2. - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla.</figcaption>
-					</figure>
-					<figure>
-						<img src="images/paperex-india-event-3.jpg" alt="event picture">
-						<figcaption>Fig3. - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla.</figcaption>
-					</figure>
-				</div>
-				<div id="menu4" class="tab-pane fade">
-					<div class="page-header">
-						<h3>2011 - Event Name</h3>
-					</div>
-					<figure>
-						<img src="images/paperex-india-event-1.jpg" alt="event picture">
-						<figcaption>Fig1. - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla.</figcaption>
-					</figure>
-					<figure>
-						<img src="images/paperex-india-event-2.jpg" alt="event picture">
-						<figcaption>Fig2. - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla.</figcaption>
-					</figure>
-					<figure>
-						<img src="images/paperex-india-event-3.jpg" alt="event picture">
-						<figcaption>Fig3. - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla.</figcaption>
-					</figure>
-				</div>
-				<div id="menu5" class="tab-pane fade">
-					<div class="page-header">
-						<h3>2010 - Event Name</h3>
-					</div>
-
-					<img src="images/GeneratedCross-0.png" alt="event picture">
-
-					<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla. Nunc sit amet erat sit amet justo aliquet vulputate non eu leo. Nam sit amet facilisis eros. Nullam maximus nunc dolor, quis hendrerit arcu rutrum sed. Maecenas eu lobortis arcu. Pellentesque egestas lectus et egestas vestibulum. Morbi placerat nunc eget tincidunt condimentum. In cursus varius ultricies. Phasellus commodo neque eu odio ullamcorper tempus. Curabitur finibus metus ac suscipit aliquet. Mauris sit amet convallis massa. Curabitur scelerisque a odio et hendrerit. Duis fringilla, massa eu cursus consectetur, massa eros eleifend dolor, in aliquam urna eros ac leo.</p>
-
-					<p class="text-justify">Nullam vestibulum est nisi, at molestie sapien pretium nec. Nulla enim massa, consequat ac lobortis nec, vestibulum nec augue. Pellentesque quis velit nec risus semper congue. Sed tincidunt in dui pulvinar bibendum. Maecenas lacinia blandit libero, eget cursus orci semper vel. Cras iaculis auctor enim a commodo. Vestibulum tincidunt ornare leo in lobortis. Etiam bibendum ornare nunc, nec iaculis nulla eleifend id. Nulla et odio laoreet odio consectetur luctus eu eget quam. Nulla ac blandit ex. Praesent lorem justo, tincidunt efficitur pharetra a, luctus eget mi.</p>
-				</div>
-			</div> <!-- /.tab-content -->
-		</div> <!-- /.row -->
-	</div>	<!-- /.container -->
+	
 
 	<script type="text/javascript" src="scripts/jquery-2.2.0.min.js"></script>
 	<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
@@ -136,13 +20,12 @@
 </body>
 </html>
 
-<!-- ************************************************************************************* -->
-<!-- *****************PHP****************************PHP********************************** -->
-<!-- ************************************************************************************* 
-<?php # Script 9.4 - view_users.php
-/* This script retrieves all the records from the users table.
 
-$page_title = 'View the Events';
+
+<?php # Script 9.4 - view_users.php
+// This script retrieves all the records from the users table.
+
+$page_title = 'View the event';
 
 
 // Page header:
@@ -151,26 +34,45 @@ echo '<h1>events</h1>';
 require ('../mysqli_connect.php'); // Connect to the db.
 		
 // Make the query:
-$q = "SELECT country, CONCAT(first_name, ' ', last_name) AS name, email, contact, country_num FROM events";		
+$q = "SELECT event_name, event_date, event_place, image_name FROM events, event_pictures";		
 $r = @mysqli_query ($dbc, $q); // Run the query.
 
 if ($r) { // If it ran OK, display the records.
 	// Fetch and print all the records:
 	while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
 		echo '
-			<div id="events-content" class="container-fluid">
-				<div class="row">
-					<div class="col-xs-12 col-md-offset-3 col-md-6 agent">
-						<img class="col-xs-4" src="styles/flag-icon-css-master/flags/4x3/'. $row['country_num'] .'.svg">
-						<div class="col-xs-8">
-							<p><b><u>'. $row['country'] . '</u></b></p>
-							<p><b>Name: </b>  <a>' . $row['name'] . '</a></p>
-							<p><b>Email Address:</b> <a>' . $row['email'] . ' </a></p>
-							<p><b>Mobile No: </b> <a>' . $row['contact'] . '</a></p>
+			<div id="event-content" class="container">
+			<div class="row">
+					<ul class="nav nav-pills nav-stacked col-xs-12 col-sm-4">
+						<li class="active">
+							<a data-toggle="pill" href="#menu0">
+								<p id="event-name">'. $row['event_name'] . ' <span class="label label-success">Coming Soon</span></p>
+								<p>'. $row['event_place'] . '</p>
+								<p>'. $row['event_date'] . '</p>
+							</a>
+						</li>
+						<li><a data-toggle="pill" href="#menu1">2014 - Event Name</a></li>
+						<li><a data-toggle="pill" href="#menu2">2013 - Event Name</a></li>
+						<li><a data-toggle="pill" href="#menu3">2012 - Event Name</a></li>
+						<li><a data-toggle="pill" href="#menu4">2011 - Event Name</a></li>
+						<li><a data-toggle="pill" href="#menu5">2010 - Event Name</a></li>					
+					</ul> <!-- /.nav -->
+
+					<div class="tab-content col-xs-12 col-sm-8">
+						<div id="menu0" class="tab-pane fade in active">
+							<div class="page-header">
+								<h3>'. $row['event_name'] . '</h3>
+							</div>
+							<figure>
+								<img src="uploads/'. $row['image_name'] . '" alt="event picture">
+								<figcaption>Fig1. - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu auctor ex, nec mattis nulla.</figcaption>
+							</figure>
+							
 						</div>
-					</div>
-				</div>
-			</div>
+						
+					</div> <!-- /.tab-content -->
+				</div> <!-- /.row -->
+			</div>	<!-- /.container -->
 		';
 	}
 
@@ -188,6 +90,6 @@ if ($r) { // If it ran OK, display the records.
 	
 } // End of if ($r) IF.
 
-mysqli_close($dbc); // Close the database connection.*/
+mysqli_close($dbc); // Close the database connection.
 
-?>-->
+?>
