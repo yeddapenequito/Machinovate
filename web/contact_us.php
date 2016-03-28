33878f6719f9264797be358c0ab59bc2639aaa4d
yeddapenequito <2013056537@ust-ics.mygbiz.com>
2016-03-28 14:35:47 +0800

    Create new marker in map!!!

--- web/contact_us.php
+++ web/contact_us.php
@@ -3,10 +3,21 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 		<link rel="stylesheet" href="./styles/bootstrap.min.css">
 		<link rel="stylesheet" href="./styles/main.css">
 		<title>Machinovate | Contact Us</title>
-	</head>
+		  <link rel="stylesheet" type="text/css"
+		    href="https://js.api.here.com/v3/3.0/mapsjs-ui.css" />
+		  <script type="text/javascript" charset="UTF-8"
+		    src="https://js.api.here.com/v3/3.0/mapsjs-core.js"></script>
+		  <script type="text/javascript" charset="UTF-8"
+		    src="https://js.api.here.com/v3/3.0/mapsjs-service.js"></script>
+		  <script type="text/javascript" charset="UTF-8"
+		    src="https://js.api.here.com/v3/3.0/mapsjs-ui.js"></script>
+		  <script type="text/javascript" charset="UTF-8"
+		    src="https://js.api.here.com/v3/3.0/mapsjs-mapevents.js"></script>
+			</head>
 	<body>
 		<?php include 'header_before_login.php';?>
 
@@ -43,7 +54,46 @@
 					<!-- <iframe src="https://embed.waze.com/iframe?zoom=12&lat=32.10911&
 lon=34.81773" width="300" height="400"></iframe> -->
 					
-					<iframe src="https://embed.waze.com/iframe?zoom=17&lat=14.72428&lon=120.98487" width="775" height="525"></iframe>
+					<div id="mapdiv">
+					<div class="row">
+						<div align=center style="width:100%; height:100%;">
+						  <div id="map" style="position:absolute; width:60%; height:325px; background:grey" ></div>
+						  <div id="panel" style="position:absolute; width:60%; left:50%; height:325px; background:inherit; " >
+						  					  
+						  
+						  </div>
+						</div>
+						  <script  type="text/javascript" charset="UTF-8" >
+
+						var platform = new H.service.Platform({
+							//Demo ID to dont change. Nawawala app id
+						  app_id: 'DemoAppId01082013GAL',
+						  app_code: 'AJKnXv84fjrb0KIHawS0Tg',
+						  useCIT: true,
+						  useHTTPS: true
+						});
+						var defaultLayers = platform.createDefaultLayers();
+						
+						var map = new H.Map(document.getElementById('map'), defaultLayers.normal.map, {
+						  //change zoom and center you know the way uhuh uhuh you like it
+						  zoom: 17.5,
+						  center: {lat: 14.724387, lng: 120.985198}
+						});
+						
+						
+						var locationsContainer = document.getElementById('panel');
+						
+						var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+						// Create the default UI components
+						var ui = H.ui.UI.createDefault(map, defaultLayers);
+						function addMarkersToMap(map) {
+						  var machinovateMarker = new H.map.Marker({lat:14.724705, lng:120.985323});
+						  map.addObject(machinovateMarker);
+						}
+						addMarkersToMap(map);
+						  </script>
+					</div>
+			</div>
 					
 				</div>
 			</div>
