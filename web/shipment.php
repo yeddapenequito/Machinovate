4b38b198bab567dd51df76a753c0b6f21c390806
Marius Sanchez <2013056549@ust-ics.mygbiz.com>
2016-03-31 04:50:24 +0800

    asdfasdf

--- web/shipment.php
+++ web/shipment.php
@@ -1,135 +1,5 @@
 <!--PHP -->
 
-<?php # Script 9.5 - register.php #2
-// This script performs an INSERT query to add a record to the shipping_details table
-
-$page_title = 'Machinovate | Add shipping details';
-
-
-// Check for form submission:
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-	require ('../mysqli_connect.php'); // Connect to the db.
-		
-	$errors = array(); // Initialize an error array.
-	
-
-	// Check for a last name:
-	if (empty($_POST['last_name'])) {
-		$errors[] = 'You forgot to enter the last name.';
-	} else {
-		$ln = mysqli_real_escape_string($dbc, trim($_POST['last_name']));
-	}
-	
-
-	// Check for a first name:
-	if (empty($_POST['first_name'])) {
-		$errors[] = 'You forgot to enter the first name.';
-	} else {
-		$fn = mysqli_real_escape_string($dbc, trim($_POST['first_name']));
-	}
-	
-	
-	// Check for a company name:
-	if (empty($_POST['company_name'])) {
-		$errors[] = 'You forgot to enter the company name.';
-	} else {
-		$cn = mysqli_real_escape_string($dbc, trim($_POST['company_name']));
-	}
-
-	// Check for an address:
-	if (empty($_POST['address'])) {
-		$errors[] = "You forgot to enter the company's address.";
-	} else {
-		$add = mysqli_real_escape_string($dbc, trim($_POST['address']));
-	}
-
-	//Check for the contact number:
-	if (empty($_POST['contact_details'])) {
-		$errors[] = 'You forgot to enter the contact details.';
-	} else {
-		$contact = mysqli_real_escape_string($dbc, trim($_POST['contact_details']));
-	}
-	
-	// Check for an email address:
-	if (empty($_POST['email'])) {
-		$errors[] = 'You forgot to enter your email address.';
-	} else {
-		$email = mysqli_real_escape_string($dbc, trim($_POST['email']));
-	}
-
-	// Check for delivery basis:
-	if (empty($_POST['delivery_basis'])) {
-		$errors[] = 'You forgot to choose your delivery basis.';
-	} else {
-		$db = mysqli_real_escape_string($dbc, trim($_POST['delivery_basis']));
-	}
-
-	// Check for shipment day:
-	if (empty($_POST['ship_day'])) {
-		$errors[] = 'You forgot to enter the shipment day.';
-	} else {
-		$sd = mysqli_real_escape_string($dbc, trim($_POST['ship_day']));
-	}
-
-	// Check for term:
-	if (empty($_POST['term'])) {
-		$errors[] = 'You forgot to choose your shipping term.';
-	} else {
-		$term = mysqli_real_escape_string($dbc, trim($_POST['term']));
-	}
-
-	$confirmation = mysqli_real_escape_string($dbc, trim($_POST['confirmation']));
-	$shipment = mysqli_real_escape_string($dbc, trim($_POST['shipment']));
-	$installation = mysqli_real_escape_string($dbc, trim($_POST['installation']));
-	
-	if (empty($errors)) { // If everything's OK.
-	
-		// Register the shipping details in the database...
-		
-		// Make the query:
-		$q = "INSERT INTO shipping_details (last_name, first_name, company_name, address, contact_details, email, delivery_basis, ship_day, term, confirmation, shipment, installation) VALUES ('$ln', '$fn', '$cn', '$add', '$contact', '$email', '$db', '$sd', '$term', '$confirmation', '$shipment', '$installation')";		
-		$r = @mysqli_query ($dbc, $q); // Run the query.
-		if ($r) { // If it ran OK.
-		
-			// Print a message:
-			echo '<h1>Thank you!</h1>
-		<p>Shipping details has been processed</p><p><br /></p>';	
-		header('Location: /Machinovate/web/summary_order.php');
-		
-		} else { // If it did not run OK.
-			
-			// Public message:
-			echo '<h1>System Error</h1>
-			<p class="error">The shipping details could not be processed due to a system error. We apologize for any inconvenience.</p>'; 
-			header('Location: /Machinovate/web/add_failed.php');
-			// Debugging message:
-			echo '<p>' . mysqli_error($dbc) . '<br /><br />Query: ' . $q . '</p>';
-						
-		} // End of if ($r) IF.
-		
-		mysqli_close($dbc); // Close the database connection.
-
-		// Include the footer and quit the script:
-		exit();
-		
-	} else { // Report the errors.
-	
-		echo '<h1>Error!</h1>
-		<p class="error">The following error(s) occurred:<br />';
-		foreach ($errors as $msg) { // Print each error.
-			echo " - $msg<br />\n";
-		}
-		echo '</p><p>Please try again.</p><p><br /></p>';
-		
-	} // End of if (empty($errors)) IF.
-	
-	mysqli_close($dbc); // Close the database connection.
-
-} // End of the main Submit conditional.
-?>
-
-
 <!DOCTYPE html>
 <html>
 	<head>
@@ -146,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			<h1 class="page-header">Shipment Details</h1>
 			<p>Please fill up necessary details.</p>
 	
-				<form role="form" action="shipment.php" method="post" 
+				<form role="form" id="slitter" method="post" 
 					role="form" class="form-horizontal">
 					<fieldset>	
 						<div class="form-group">
@@ -284,8 +154,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 				
 					
 		</div>
+
 		<script type="text/javascript" src="scripts/jquery-2.2.0.min.js"></script>
 		<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
+		<script type="text/javascript" src="actionlistener.js"></script>
 		<script type="text/javascript">
 
 			document.getElementById("transfer").onchange = function() {
