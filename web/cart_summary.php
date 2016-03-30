bac96c1a4bd23351b946cc248787703041609fb7
Nyries <2013056563@ust-ics.mygbiz.com>
2016-03-30 15:41:27 +0800

    Remove "Your Shopping Cart" button on cart_summary.php

--- web/cart_summary.php
+++ web/cart_summary.php
@@ -239,5 +239,8 @@
 				</div>
 				<script type="text/javascript" src="scripts/jquery-2.2.0.min.js"></script>
 				<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
+				<script>	
+					document.getElementById("your-shopping-cart-btn").remove();
+				</script>
 			</body>
 			</html>
