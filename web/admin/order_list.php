5092060f675aab5f72fc72ef88bbca18478d40d1
Nyries <2013056563@ust-ics.mygbiz.com>
2016-03-31 10:56:45 +0800

    Add delete dialog box

--- web/admin/order_list.php
+++ web/admin/order_list.php
@@ -187,7 +187,24 @@
 							</div> <!-- /.modal -->
 		    			</td>
 		    			<td>
-		    				<button><span class="glyphicon glyphicon-remove"></span></button>
+		    				<button data-toggle="modal" href='#delete'><span class="glyphicon glyphicon-remove"></span></button>
+		    				<div class="modal fade" id="delete">
+		    					<div class="modal-dialog">
+		    						<div class="modal-content">
+		    							<div class="modal-header">
+		    								<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+		    								<h4 class="modal-title text-danger">Delete</h4>
+		    							</div>
+		    							<div class="modal-body">
+		    								<p class="text-danger">Are your sure you want to delete Order No. 0001</p>
+		    							</div>
+		    							<div class="modal-footer">
+		    								<button type="button" class="btn btn-default" data-dismiss="modal">No</button>
+		    								<button type="button" class="btn btn-primary">Yes</button>
+		    							</div>
+		    						</div>
+		    					</div>
+		    				</div>
 		    			</td>
 		    		</tr>
 		    		<tr>
