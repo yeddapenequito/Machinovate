742071dcc0a3a9a4d0cff12203adf317f4103123
Nyries <2013056563@ust-ics.mygbiz.com>
2016-04-10 17:33:26 +0800

    Reindent shipment.php and resolve unpaired tags

--- web/shipment.php
+++ web/shipment.php
@@ -16,141 +16,139 @@
 			<h1 class="page-header">Shipment Details</h1>
 			<p class="text-danger">All fields are required.</p>
 	
-				<form role="form" id="slitter" method="post" 
-					role="form" class="form-horizontal">
-					<fieldset>	
-						<div class="form-group">
-
-							<label class="col-sm-2 control-label">
-								Name:</label>
-							<div class="col-sm-10">
-								<div class="form-inline">	
-							 		<input autofocus class="form-control" required name="last_name" placeholder="Last Name" type="text">
-							 		<input class="form-control" name="first_name" placeholder="First Name" required type="text"> 
-								</div>	
-							</div>
-						</div>
-
-						<div class="form-group">
-
-							<label class="col-sm-2 control-label">
-								Company Name:</label>
-							<div class="col-sm-10">	
-								<input class="form-control" name="company_name" required type="text"> 
-							</div>
-						</div>
-						
-						<div class="form-group">
-
-							<label class="col-sm-2 control-label">
-								Address:</label>
-							<div class="col-sm-10">	
-								<input class="form-control" name="address" placeholder="[Building House_No Street_Name District_Name] Barangay, Municipality/Province" required type="text"> 
-							</div>
-						</div>
-					
-						<div class="form-group">
-
-							<label class="col-sm-2 control-label">
-								Mobile Number:</label>
-							<div class="col-sm-10">	
-								<input class="form-control" name="contact_details" required type="text"> 
-							</div>
-						</div>
-					
-						<div class="form-group">
-
-							<label class="col-sm-2 control-label">
-								Email Address:</label>
-							<div class="col-sm-10">	
-								<input class="form-control" name="email" required type="email"> 
-							</div>
-						</div>
-					</fieldset>
-
-					<!-- Shipping Terms Section -->
-					<fieldset>
-						<legend>Shipping Terms Section:</legend>
-
-						<!-- Shipping Delivery Basis -->
-						<div class="form-group">
-							<label class="control-label col-sm-2" for="ship-basis">
-								Shipping Delivery Basis: </label>
-							<div class="col-sm-10">
-								<div class="radio">
-									<label><input type="radio" name="delivery_basis" value="Free On Board">
-										Free on Board (FOB) </label>
-								</div>
-								<div class="radio">
-									<label><input type="radio" name="delivery_basis" value="Cost Insurance and Freight">
-										Cost, Insurance and Freight (CIF) </label>
-								</div>
-								<div class="radio">
-									<label><input type="radio" name="delivery_basis" value="Cost and Freight">
-										Cost and Freight </label>
-								</div>
-							</div>
-						</div>	
-
-						<!--Shipment -->
-						<div class="form-group">
-
-							<label class="control-label col-sm-2" for="shipment">
-								Shipment: </label>
-							<div class="col-sm-10">
-								<div class="input-group">
-									<input class="form-control"  placeholder="No. of Days" min="1" type="number" name="ship_day"  required>
-									<span class="input-group-addon">Working days after initial payment.</span>
-								</div>
-							</div>
-						</div>
-
-						<!-- Payment Terms -->
-						<div class="form-group">
-
-							<label class="control-label col-sm-2" for="payment">
-								Payment Terms: </label>
-							<div class="col-sm-10">
-								<div class="checkbox">
-									<label><input type="checkbox" name="term" id= "credit" value="Letter of Credit, Draft at Sight"> Letter of Credit, Draft at Sight </label>
-								</div>
-
-								<div class="checkbox">
-									<label><input type="checkbox" name="term" id="transfer" value="Telegraphic Transfer">Telegraphic Transfer </label>
-								</div>
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-sm-offset-2 col-sm-10">
-								<div class="input-group">
-									<input class="form-control" type="number" name="confirmation" required/>
-									<span class="input-group-addon">% downpayment upon confirmation</span>
-								</div>
-
-								<div class="input-group">
-									<input class="form-control" type="number" name="shipment" required/>
-									<span class="input-group-addon">% before shipment</span>
-								</div>
-
-								
-							</div>
-						</div>
-					</fieldset>		
-
-					<!-- Submit Button -->
+			<form role="form" id="slitter" method="post" 
+				role="form" class="form-horizontal">
+				<fieldset>	
 					<div class="form-group">
-					<div class="col-sm-offset-2 col-sm-10">
-					<button class="btn btn-primary" value="Submit" type="submit">
-							Submit</button>
-					<a class="btn btn-default" id="cancel-btn" href="machines.php">
-							Cancel</a>
-						
+
+						<label class="col-sm-2 control-label">
+							Name:</label>
+						<div class="col-sm-10">
+							<div class="form-inline">	
+						 		<input autofocus class="form-control" required name="last_name" placeholder="Last Name" type="text">
+						 		<input class="form-control" name="first_name" placeholder="First Name" required type="text"> 
+							</div>	
+						</div>
 					</div>
-					</form>
-				
+
+					<div class="form-group">
+
+						<label class="col-sm-2 control-label">
+							Company Name:</label>
+						<div class="col-sm-10">	
+							<input class="form-control" name="company_name" required type="text"> 
+						</div>
+					</div>
 					
-		</div>
+					<div class="form-group">
+
+						<label class="col-sm-2 control-label">
+							Address:</label>
+						<div class="col-sm-10">	
+							<input class="form-control" name="address" placeholder="[Building House_No Street_Name District_Name] Barangay, Municipality/Province" required type="text"> 
+						</div>
+					</div>
+				
+					<div class="form-group">
+
+						<label class="col-sm-2 control-label">
+							Mobile Number:</label>
+						<div class="col-sm-10">	
+							<input class="form-control" name="contact_details" required type="text"> 
+						</div>
+					</div>
+				
+					<div class="form-group">
+
+						<label class="col-sm-2 control-label">
+							Email Address:</label>
+						<div class="col-sm-10">	
+							<input class="form-control" name="email" required type="email"> 
+						</div>
+					</div>
+				</fieldset>
+
+				<!-- Shipping Terms Section -->
+				<fieldset>
+					<legend>Shipping Terms Section:</legend>
+
+					<!-- Shipping Delivery Basis -->
+					<div class="form-group">
+						<label class="control-label col-sm-2" for="ship-basis">
+							Shipping Delivery Basis: </label>
+						<div class="col-sm-10">
+							<div class="radio">
+								<label><input type="radio" name="delivery_basis" value="Free On Board">
+									Free on Board (FOB) </label>
+							</div>
+							<div class="radio">
+								<label><input type="radio" name="delivery_basis" value="Cost Insurance and Freight">
+									Cost, Insurance and Freight (CIF) </label>
+							</div>
+							<div class="radio">
+								<label><input type="radio" name="delivery_basis" value="Cost and Freight">
+									Cost and Freight </label>
+							</div>
+						</div>
+					</div>	
+
+					<!--Shipment -->
+					<div class="form-group">
+
+						<label class="control-label col-sm-2" for="shipment">
+							Shipment: </label>
+						<div class="col-sm-10">
+							<div class="input-group">
+								<input class="form-control"  placeholder="No. of Days" min="1" type="number" name="ship_day"  required>
+								<span class="input-group-addon">Working days after initial payment.</span>
+							</div>
+						</div>
+					</div>
+
+					<!-- Payment Terms -->
+					<div class="form-group">
+
+						<label class="control-label col-sm-2" for="payment">
+							Payment Terms: </label>
+						<div class="col-sm-10">
+							<div class="checkbox">
+								<label><input type="checkbox" name="term" id= "credit" value="Letter of Credit, Draft at Sight"> Letter of Credit, Draft at Sight </label>
+							</div>
+
+							<div class="checkbox">
+								<label><input type="checkbox" name="term" id="transfer" value="Telegraphic Transfer">Telegraphic Transfer </label>
+							</div>
+						</div>
+					</div>
+
+					<div class="form-group">
+						<div class="col-sm-offset-2 col-sm-10">
+							<div class="input-group">
+								<input class="form-control" type="number" name="confirmation" required/>
+								<span class="input-group-addon">% downpayment upon confirmation</span>
+							</div>
+
+							<div class="input-group">
+								<input class="form-control" type="number" name="shipment" required/>
+								<span class="input-group-addon">% before shipment</span>
+							</div>
+
+							
+						</div>
+					</div>
+				</fieldset>		
+
+				<!-- Submit Button -->
+				<div class="form-group">
+					<div class="col-sm-offset-2 col-sm-10">
+						<button class="btn btn-primary" value="Submit" type="submit">
+								Submit</button>
+						<a class="btn btn-default" id="cancel-btn" href="machines.php">
+								Cancel</a>
+					</div>
+				</div>
+			</form>
+		</div> <!-- /.container -->
 
 		<script type="text/javascript" src="scripts/jquery-2.2.0.min.js"></script>
 		<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
