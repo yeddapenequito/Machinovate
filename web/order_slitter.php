6d2ade4af407a812445c8d9ba620c0402ed6c409
Nyries <2013056563@ust-ics.mygbiz.com>
2016-05-06 22:32:12 +0800

    Uncomment unit options field on slitter order form

--- web/order_slitter.php
+++ web/order_slitter.php
@@ -125,10 +125,10 @@
 
 								<input class="form-control" min="0.001" name="gsmMax" placeholder="Max" size="5" step="0.001" type="number" required value='<?php setStickyText("gsm_max");?>'/>
 
-								<!-- <select class="form-control" name="gsmUnit"> 
+								<select class="form-control" name="gsmUnit"> 
 									<option value="inches">inches</option>
 									<option value="mm">millimeters</option>
-								</select> -->
+								</select>
 								<div class="help-block with-errors"></div>
 							</div>
 						</div>
