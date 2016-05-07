7a61c7425d5151b10f5430fcf1c6850b16c8bf02
yeddapenequito <2013056537@ust-ics.mygbiz.com>
2016-05-07 23:11:24 +0800

    Add cancel and proceed to checkout button

--- web/cart_summary.php
+++ web/cart_summary.php
@@ -190,6 +190,8 @@
 			<br>
 			<br>
 
+				<a id="cancel-btn" href="machines.php" class="btn btn-default">Cancel Order</a>
+				<a id="add-to-cart-btn" href="shipment.php"type="submit" class="btn btn-default">Proceed to Checkout</a>
 					</div>
 				</div>
 				<script type="text/javascript" src="scripts/jquery.min.js"></script>
