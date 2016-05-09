9a37f6fd6c35d3765864ba2103113670db4e73c3
yeddapenequito <2013056537@ust-ics.mygbiz.com>
2016-05-09 23:44:38 +0800

    Add events successful page

---
+++ web/events_successful.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="../styles/bootstrap.min.css">
+	<link rel="stylesheet" href="../styles/main.css">
+	<link rel="stylesheet" href="../styles/admin.css">
+	<title>Machinovate | Add Events</title>
+</head>
+<body>
+	<?php include 'header_after_login.php';?>
+	
+	<div class="container">
+		<h1>Thank you!</h1>
+		<h3>An event has been successfully registered.</h3>
+		<br/>
+		<a class="btn btn-default" href="add_events.php">Add another event</a>
+		or  
+		<a class="btn btn-default" href="order_list.php">Go back to Order List</a>		
+	</div> <!-- /.container -->
+	<script type="text/javascript" src="../scripts/jquery.min.js"></script>
+	<script type="text/javascript" src="../scripts/bootstrap.min.js"></script>
+</body>
+</html>
